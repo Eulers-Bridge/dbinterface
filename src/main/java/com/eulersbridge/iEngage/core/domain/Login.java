363e32010bd111d18f6eec4a49e42f65ea274e59
Greg Newitt <greg.newitt@eulersbridge.com>
2014-06-16 15:52:20 +1000

    Added Logging

--- src/main/java/com/eulersbridge/iEngage/core/domain/Login.java
+++ src/main/java/com/eulersbridge/iEngage/core/domain/Login.java
@@ -1,30 +1,44 @@
 package com.eulersbridge.iEngage.core.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.eulersbridge.iEngage.rest.domain.Response;
 
-public class Login {
+public class Login 
+{
     private final long id;
     private final String username;
     private final String content;
 
-    public Login(long id, String username, String content) {
+    private static Logger LOG = LoggerFactory.getLogger(Login.class);
+
+    public Login(long id, String username, String content) 
+    {
+    	if (LOG.isTraceEnabled()) LOG.trace("constructor("+id+','+username+','+content+')');
         this.id = id;
         this.content = content;
         this.username = username;
     }
 
-	public long getId() {
+	public long getId() 
+	{
+    	if (LOG.isDebugEnabled()) LOG.debug("getId() = "+id);
         return id;
     }
 
     public String getUsername() {
+    	if (LOG.isDebugEnabled()) LOG.debug("getUsername() = "+username);
         return username;
     }
     public String getContent() {
+    	if (LOG.isDebugEnabled()) LOG.debug("getContent() = "+content);
         return content;
     }
 
-	public Response process() {
+	public Response process() 
+	{
+    	if (LOG.isDebugEnabled()) LOG.debug("process() = ");
 		// TODO Auto-generated method stub
 		return new Response(true,username+" logged in");
 	}
--- src/main/java/com/eulersbridge/iEngage/core/domain/Logout.java
+++ src/main/java/com/eulersbridge/iEngage/core/domain/Logout.java
@@ -1,5 +1,8 @@
 package com.eulersbridge.iEngage.core.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.eulersbridge.iEngage.rest.domain.Response;
 
 public class Logout {
@@ -8,21 +11,30 @@ public class Logout {
     private final String username;
     private static final String logoutTemplate = " logged out.";
 
-    public Logout(long id, String content) {
+    private static Logger LOG = LoggerFactory.getLogger(Logout.class);
+
+    public Logout(long id, String username) 
+    {
+    	if (LOG.isTraceEnabled()) LOG.trace("constructor("+id+','+','+username+')');
         this.id = id;
-        this.username = content;
+        this.username = username;
     }
 
-    public long getId() {
+    public long getId() 
+    {
+    	if (LOG.isDebugEnabled()) LOG.debug("getId() = "+id);
         return id;
     }
 
-    public String getUsername() {
+    public String getUsername() 
+    {
+    	if (LOG.isDebugEnabled()) LOG.debug("getUsername() = "+username);
         return username;
     }
 
 	public Response process() 
 	{
+    	if (LOG.isDebugEnabled()) LOG.debug("process() = ");
 		// TODO Auto-generated method stub
 		return new Response(true,username+logoutTemplate);
 	}
