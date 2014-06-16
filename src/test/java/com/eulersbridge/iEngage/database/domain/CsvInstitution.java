590a0e33672a173c9cc3917378af1e9997d3eb7a
Greg Newitt <greg.newitt@eulersbridge.com>
2014-06-16 15:27:05 +1000

    Added Logging

--- src/test/java/com/eulersbridge/iEngage/database/domain/CsvInstitution.java
+++ src/test/java/com/eulersbridge/iEngage/database/domain/CsvInstitution.java
@@ -23,10 +23,8 @@ import org.supercsv.io.ICsvListReader;
 import org.supercsv.io.ICsvMapReader;
 import org.supercsv.prefs.CsvPreference;
 
-import com.eulersbridge.iEngage.rest.controller.LoginController;
-
 public class CsvInstitution extends Institution {
-    private static Logger LOG = LoggerFactory.getLogger(LoginController.class);
+    private static Logger LOG = LoggerFactory.getLogger(CsvInstitution.class);
 
 	public CsvInstitution() {
 	}
@@ -39,7 +37,9 @@ public class CsvInstitution extends Institution {
 	 * @return the cell processors
 	 */
 
-	public static CellProcessor[] getProcessors() {
+	public static CellProcessor[] getProcessors() 
+	{
+		if (LOG.isDebugEnabled()) LOG.debug("getProcessors() entered");
 	        
 	        final String emailRegex = "[a-z0-9\\._]+@[a-z0-9\\.]+"; // just an example, not very robust!
 	        StrRegEx.registerMessage(emailRegex, "must be a valid email address");
@@ -65,105 +65,137 @@ public class CsvInstitution extends Institution {
 	        return processors;
 	}
 	
-	public static void readWithCsvBeanReader(Reader reader) throws Exception {
-        
+	public static void readWithCsvBeanReader(Reader reader) throws Exception 
+	{
+		if (LOG.isDebugEnabled()) LOG.debug("readWithCsvBeanReader() entered");
+    
         ICsvBeanReader beanReader = null;
-        try {
-                beanReader = new CsvBeanReader(reader, CsvPreference.STANDARD_PREFERENCE);
-                
-                // the header elements are used to map the values to the bean (names must match)
-                final String[] header = beanReader.getHeader(true);
-                final CellProcessor[] processors = getProcessors();
-                
-                Institution inst;
-                while( (inst = beanReader.read(Institution.class, header, processors)) != null ) 
-                {
-                	if (LOG.isDebugEnabled())
-                		LOG.debug(String.format("lineNo=%s, rowNo=%s, institution=%s", beanReader.getLineNumber(),
-	                            beanReader.getRowNumber(), inst));
-	                    System.out.println(String.format("CREATE ({name:'%s', Campus:'%s', State:'%s', Country:'%s'})", 
-	                            inst.getName(), inst.getCampus(), inst.getState(), inst.getCountry()));
-                }
-                
+        try 
+        {
+            beanReader = new CsvBeanReader(reader, CsvPreference.STANDARD_PREFERENCE);
+            
+            // the header elements are used to map the values to the bean (names must match)
+            final String[] header = beanReader.getHeader(true);
+            final CellProcessor[] processors = getProcessors();
+            
+            Institution inst;
+            while( (inst = beanReader.read(Institution.class, header, processors)) != null ) 
+            {
+            	if (LOG.isDebugEnabled())
+            		LOG.debug(String.format("lineNo=%s, rowNo=%s, institution=%s", beanReader.getLineNumber(),
+                            beanReader.getRowNumber(), inst));
+                System.out.println(String.format("CREATE ({name:'%s', Campus:'%s', State:'%s', Country:'%s'})", 
+                            inst.getName(), inst.getCampus(), inst.getState(), inst.getCountry()));
+            }
+            
         }
-        finally {
-                if( beanReader != null ) {
-                        beanReader.close();
-                }
+        finally 
+        {
+            if( beanReader != null ) 
+            {
+                    beanReader.close();
+            }
         }
 	}
-	public static void readWithCsvBeanReader2(String fileName) throws Exception {
+	public static void readWithCsvBeanReader2(String fileName) throws Exception 
+	{
+		if (LOG.isDebugEnabled()) LOG.debug("readWithCsvBeanReader2() entered");
         
         ICsvBeanReader beanReader = null;
-        try {
-                beanReader = new CsvBeanReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE);
-                
-                // the header elements are used to map the values to the bean (names must match)
-                final String[] header = beanReader.getHeader(true);
-                final CellProcessor[] processors = getProcessors();
-                
-                Institution customer;
-                while( (customer = beanReader.read(Institution.class, header, processors)) != null ) {
-                        System.out.println(String.format("lineNo=%s, rowNo=%s, institution=%s", beanReader.getLineNumber(),
-                                beanReader.getRowNumber(), customer));
-                }
-                
+        try 
+        {
+            beanReader = new CsvBeanReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE);
+            
+            // the header elements are used to map the values to the bean (names must match)
+            final String[] header = beanReader.getHeader(true);
+            final CellProcessor[] processors = getProcessors();
+            
+            Institution inst;
+            while( (inst = beanReader.read(Institution.class, header, processors)) != null ) 
+            {
+	        	if (LOG.isDebugEnabled())
+	        		LOG.debug(String.format("lineNo=%s, rowNo=%s, institution=%s", beanReader.getLineNumber(),
+	                        beanReader.getRowNumber(), inst));
+	            System.out.println(String.format("lineNo=%s, rowNo=%s, institution=%s", beanReader.getLineNumber(),
+	                        beanReader.getRowNumber(), inst));
+            }
+            
         }
-        finally {
-                if( beanReader != null ) {
-                        beanReader.close();
-                }
+        finally 
+        {
+            if( beanReader != null ) 
+            {
+                beanReader.close();
+            }
         }
 	}
 	/**
 	 * An example of reading using CsvListReader.
 	 */
-	public static void readWithCsvListReader(String fileName) throws Exception {
+	public static void readWithCsvListReader(String fileName) throws Exception 
+	{
 	        
-	        ICsvListReader listReader = null;
-	        try {
-	                listReader = new CsvListReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE);
-	                
-	                listReader.getHeader(true); // skip the header (can't be used with CsvListReader)
-	                final CellProcessor[] processors = getProcessors();
-	                
-	                List<Object> customerList;
-	                while( (customerList = listReader.read(processors)) != null ) {
-	                        System.out.println(String.format("lineNo=%s, rowNo=%s, customerList=%s", listReader.getLineNumber(),
-	                                listReader.getRowNumber(), customerList));
-	                }
-	                
-	        }
-	        finally {
-	                if( listReader != null ) {
-	                        listReader.close();
-	                }
-	        }
+		if (LOG.isDebugEnabled()) LOG.debug("readWithCsvListReader() entered");
+	
+		ICsvListReader listReader = null;
+	    try 
+	    {
+            listReader = new CsvListReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE);
+            
+            listReader.getHeader(true); // skip the header (can't be used with CsvListReader)
+            final CellProcessor[] processors = getProcessors();
+            
+            List<Object> instList;
+            while( (instList = listReader.read(processors)) != null ) 
+            {
+	        	if (LOG.isDebugEnabled())
+	        		LOG.debug(String.format("lineNo=%s, rowNo=%s, institution=%s", listReader.getLineNumber(),
+	                        listReader.getRowNumber(), instList));
+                System.out.println(String.format("lineNo=%s, rowNo=%s, customerList=%s", listReader.getLineNumber(),
+                        listReader.getRowNumber(), instList));
+            }
+            
+	    }
+        finally 
+        {
+            if( listReader != null ) 
+            {
+                listReader.close();
+            }
+        }
 	}
 	/**
 	 * An example of reading using CsvMapReader.
 	 */
-	public static void readWithCsvMapReader(String fileName) throws Exception {
-	        
-	        ICsvMapReader mapReader = null;
-	        try {
-	                mapReader = new CsvMapReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE);
-	                
-	                // the header columns are used as the keys to the Map
-	                final String[] header = mapReader.getHeader(true);
-	                final CellProcessor[] processors = getProcessors();
-	                
-	                Map<String, Object> customerMap;
-	                while( (customerMap = mapReader.read(header, processors)) != null ) {
-	                        System.out.println(String.format("lineNo=%s, rowNo=%s, customerMap=%s", mapReader.getLineNumber(),
-	                                mapReader.getRowNumber(), customerMap));
-	                }
-	                
-	        }
-	        finally {
-	                if( mapReader != null ) {
-	                        mapReader.close();
-	                }
-	        }
+	public static void readWithCsvMapReader(String fileName) throws Exception 
+	{
+		if (LOG.isDebugEnabled()) LOG.debug("readWithCsvListReader() entered");
+        
+		ICsvMapReader mapReader = null;
+        try 
+        {
+            mapReader = new CsvMapReader(new FileReader(fileName), CsvPreference.STANDARD_PREFERENCE);
+            
+            // the header columns are used as the keys to the Map
+            final String[] header = mapReader.getHeader(true);
+            final CellProcessor[] processors = getProcessors();
+            
+            Map<String, Object> instMap;
+            while( (instMap = mapReader.read(header, processors)) != null )
+            {
+	        	if (LOG.isDebugEnabled())
+	        		LOG.debug(String.format("lineNo=%s, rowNo=%s, institution=%s", mapReader.getLineNumber(),
+	                        mapReader.getRowNumber(), instMap));
+                System.out.println(String.format("lineNo=%s, rowNo=%s, customerMap=%s", mapReader.getLineNumber(),
+                    mapReader.getRowNumber(), instMap));
+            }
+        }
+        finally 
+        {
+            if( mapReader != null ) 
+            {
+                mapReader.close();
+            }
+        }
 	}
 }
