0de6275c5e949635722dbc609bcaae86df6d3da7
Greg Newitt <greg.newitt@eulersbridge.com>
2014-06-23 09:46:56 +1000

    Added some extra configure methods

--- src/main/java/com/eulersbridge/iEngage/config/SecurityConfig.java
+++ src/main/java/com/eulersbridge/iEngage/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
@@ -28,11 +26,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	    auth.inMemoryAuthentication()
         .withUser("gnewitt").password("test").roles("USER");
 	}
-
+	
 /*	@Override
+	public void configure(WebSecurity webSec) throws Exception
+	{
+		
+	}
+
+	@Override
 	protected void configure(HttpSecurity http) throws Exception
 	{
-		String loginPage="/login";
+		String loginPage="/general-info";
 		http.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin().loginPage(loginPage).permitAll();
-	} */
+	}
+*/	
 }
