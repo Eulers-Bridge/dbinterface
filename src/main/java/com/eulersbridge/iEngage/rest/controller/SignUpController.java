cc88c3b5a7fdb7a05a2a40358e6a9989be5d4ff3
Greg Newitt <greg.newitt@eulersbridge.com>
2014-06-27 20:56:59 +1000

    Added signUp controller

--- src/main/java/com/eulersbridge/iEngage/rest/controller/SignUpController.java
+++ src/main/java/com/eulersbridge/iEngage/rest/controller/SignUpController.java
@@ -4,6 +4,8 @@ package com.eulersbridge.iEngage.rest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,18 +28,21 @@ public class SignUpController {
     private static Logger LOG = LoggerFactory.getLogger(ElectionController.class);
 
 
-    @RequestMapping(value="/signUp/{email}")
+    @RequestMapping(method=RequestMethod.POST,value="/signUp/")
     public @ResponseBody Student saveNewUser(
-            @PathVariable String email) 
+            @RequestParam(value="email", required=true) String email,
+            @RequestParam(value="firstName", required=true) String firstName,
+            @RequestParam(value="lastName", required=true) String lastName,
+            @RequestParam(value="gender", required=true) String gender,
+            @RequestParam(value="nationality", required=true) String nationality,
+            @RequestParam(value="yearOfBirth", required=true) String yearOfBirth,
+            @RequestParam(value="personality", required=true) String personality,
+            @RequestParam(value="password", required=true) String password,
+            @RequestParam(value="institutionId", required=true) Long institutionId
+            ) 
     {
     	if (LOG.isInfoEnabled()) LOG.info(email+" attempting to save student. ");
-    	String yearOfBirth="";
-		String gender="";
-		String personality="";
-		String lastName="test";
-		String firstName="test";
-		String nationality="Oz";
-		Student student=new Student(email,lastName,firstName,personality,nationality,yearOfBirth,gender);
+		Student student=new Student(email,lastName,firstName,personality,nationality,yearOfBirth,gender,password);
     	Student test = repo.save(student);
 		if (LOG.isDebugEnabled()) LOG.debug("test = "+test);
 		if (LOG.isDebugEnabled()) LOG.debug("Count = "+repo.count());
