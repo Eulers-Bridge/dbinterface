e5a278015cd99a724d50386ad7de651975a4acec
Greg Newitt <greg.newitt@eulersbridge.com>
2014-06-29 22:10:05 +1000

    Came up with working version of SignUp controller insofar as data being inserted into DB

--- src/main/java/com/eulersbridge/iEngage/rest/controller/SignUpController.java
+++ src/main/java/com/eulersbridge/iEngage/rest/controller/SignUpController.java
@@ -3,9 +3,9 @@ package com.eulersbridge.iEngage.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,28 +31,59 @@ public class SignUpController {
     private static Logger LOG = LoggerFactory.getLogger(ElectionController.class);
 
 
-    @RequestMapping(method=RequestMethod.PUT,value="/signUp")
+/**
+ * Is passed all the necessary data to create a new user.
+ * The request must be a PUT with the necessary parameters in the
+ * attached data.
+ * <p/>
+ * This method will return the resulting user object which will now also have a userID
+ * supplied by the Graph Database.  There will also be a relationship set up with the 
+ * institution the user belongs to.
+ * 
+ * @param user the user object passed across as JSON.
+ * @param institutionId the institution this user belongs to.
+ * @return the user object returned by the Graph Database.
+ * 
+
+		*/
+    @RequestMapping(method=RequestMethod.PUT,value="/signUp/{institutionId}")
     public @ResponseBody Student saveNewUser(
-            @RequestParam(value="email", required=true) String email,
-            @RequestParam(value="firstName", required=true) String firstName,
-            @RequestParam(value="lastName", required=true) String lastName,
-            @RequestParam(value="gender", required=true) String gender,
-            @RequestParam(value="nationality", required=true) String nationality,
-            @RequestParam(value="yearOfBirth", required=true) String yearOfBirth,
-            @RequestParam(value="personality", required=true) String personality,
-            @RequestParam(value="password", required=true) String password,
-            @RequestParam(value="institutionId", required=true) Long institutionId
+            @RequestBody Student user,
+            @PathVariable Long institutionId
             ) 
     {
-    	if (LOG.isInfoEnabled()) LOG.info(email+" attempting to save student. ");
-		Student student=new Student(email,lastName,firstName,personality,nationality,yearOfBirth,gender,password);
+    	if (LOG.isInfoEnabled()) LOG.info("attempting to save user "+user);
     	Institution inst=instRepo.findOne(institutionId);
-		student.setInstitution(inst);
-		Student test = studentRepo.save(student);
-		if (LOG.isDebugEnabled()) LOG.debug("test = "+test);
-		if (LOG.isDebugEnabled()) LOG.debug("Count = "+studentRepo.count());
-		Student result = studentRepo.findOne(test.getNodeId());
+    	Student result;
+    	if (inst!=null)
+    	{
+    		user.setInstitution(inst);
+    		Student test = studentRepo.save(user);
+    		if (LOG.isDebugEnabled()) LOG.debug("test = "+test);
+    		if (LOG.isDebugEnabled()) LOG.debug("Count = "+studentRepo.count());
+    		result = studentRepo.findOne(test.getNodeId());
+    	}
+    	else
+    	{
+    		result=user;
+    	}
     	return result;
+    }
+    
+    
+    @RequestMapping(value="/displayParams/{institutionId}")
+    public @ResponseBody boolean displayDetails(
+            @RequestBody Student user,
+            @PathVariable Long institutionId
+            ) 
+    {
+    	if (LOG.isInfoEnabled()) 
+    	{
+    		LOG.info("user = "+user);
+    		LOG.info("password = "+user.getPassword());
+    		LOG.info("institutionId = "+institutionId);
+    	}
+    	return true;
     }
     
     
