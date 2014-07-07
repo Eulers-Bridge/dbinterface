51a915aac09ba4a67db7dc9ac092686ba685c40a
Greg Newitt <greg.newitt@eulersbridge.com>
2014-07-07 14:44:40 +1000

    Implementation of the UserService interface.

--- src/main/java/com/eulersbridge/iEngage/core/services/UserEventHandler.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/UserEventHandler.java
@@ -1,10 +1,16 @@
 package com.eulersbridge.iEngage.core.services;
 
+import java.util.Iterator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.neo4j.conversion.Result;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.eulersbridge.iEngage.core.events.users.ReadUserEvent;
+import com.eulersbridge.iEngage.core.events.users.RequestReadUserEvent;
+import com.eulersbridge.iEngage.core.events.users.UserDetails;
 import com.eulersbridge.iEngage.database.domain.Institution;
 import com.eulersbridge.iEngage.database.domain.User;
 import com.eulersbridge.iEngage.database.repository.InstitutionRepository;
@@ -12,24 +18,37 @@ import com.eulersbridge.iEngage.database.repository.UserRepository;
 
 public class UserEventHandler implements UserService {
 
-	@Autowired UserRepository studentRepo;
-	@Autowired InstitutionRepository instRepo;
+//	@Autowired InstitutionRepository instRepo;
 
-    private static Logger LOG = LoggerFactory.getLogger(User.class);
+    private static Logger LOG = LoggerFactory.getLogger(UserEventHandler.class);
 
+    private UserRepository userRepository;
+
+    public UserEventHandler(final UserRepository userRepository) {
+      this.userRepository = userRepository;
+    }
+    
+/*	public UserEventHandler() 
+	{
+		// TODO Auto-generated constructor stub
+	}
+*/
 	@Override
 	@Transactional
-	public User signUpNewUser(User newUser, Long institutionId) 
+	public User signUpNewUser(UserDetails newUser) 
 	{
-    	Institution inst=instRepo.findOne(institutionId);
-    	User result=newUser;
-    	if (inst!=null)
+//    	Institution inst=instRepo.findOne(newUser.getInstitution().getNodeId());
+    	User userToInsert=User.fromUserDetails(newUser);
+//    	userToInsert.setInstitution(inst);
+    	
+    	User result=null;
+//    	if (inst!=null)
     	{
-    		newUser.setInstitution(inst);
+//    		newUser.setInstitution(inst);
 /*    		Transaction tx=graphDatabaseService.beginTx();
     		try
     		{
-*/    			User test = studentRepo.save(newUser);
+*/    			User test = userRepository.save(userToInsert);
 /*    			tx.success();
     		}
     		catch (Exception e)
@@ -41,17 +60,43 @@ public class UserEventHandler implements UserService {
     			tx.finish();
     		}
 */    		if (LOG.isDebugEnabled()) LOG.debug("test = "+test);
-    		if (LOG.isDebugEnabled()) LOG.debug("Count = "+studentRepo.count());
-    		result = studentRepo.findOne(test.getNodeId());
+    		if (LOG.isDebugEnabled()) LOG.debug("Count = "+userRepository.count());
+    		result = userRepository.findOne(test.getNodeId());
     	}
     	return result;
 	}
 
 	@Override
-	public User findUserByEmail(String emailAddress) 
+	public User findUser(String email) 
 	{
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public ReadUserEvent requestReadUser(RequestReadUserEvent requestReadUserEvent) {
+//TODO	    User user = userRepository.findByEmail(requestReadUserEvent.getEmail());
+	    if (LOG.isDebugEnabled()) LOG.debug("requestReadUser("+requestReadUserEvent.getEmail()+")");
+		Result <User> users = userRepository.findAll();
+		Iterator<User> iter=users.iterator();
+		User user=null;
+		while (iter.hasNext())
+		{
+			User res=iter.next();
+			if (res.getEmail().equals(requestReadUserEvent.getEmail()))
+			{	
+				user=res;
+				if (LOG.isDebugEnabled()) LOG.debug("res = "+res);
+			}
+		}
+
+
+	    if (user == null) {
+	      return ReadUserEvent.notFound(requestReadUserEvent.getEmail());
+	    }
+	    UserDetails result=user.toUserDetails();
+	    if (LOG.isDebugEnabled()) LOG.debug("Result - "+result);
+	    return new ReadUserEvent(requestReadUserEvent.getEmail(), result);
 	}
 
 }
