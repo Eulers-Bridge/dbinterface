eb1559de2a6ee39b675647cda8a54a903cb0fbab
Greg Newitt <greg.newitt@eulersbridge.com>
2014-07-07 14:42:43 +1000

    Using UserService to abstract data access.

--- src/main/java/com/eulersbridge/iEngage/rest/controller/UserController.java
+++ src/main/java/com/eulersbridge/iEngage/rest/controller/UserController.java
@@ -1,69 +1,62 @@
 package com.eulersbridge.iEngage.rest.controller;
 
-import java.util.Iterator;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.neo4j.conversion.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eulersbridge.iEngage.database.domain.User;
-import com.eulersbridge.iEngage.database.repository.UserRepository;
+import com.eulersbridge.iEngage.core.events.users.ReadUserEvent;
+import com.eulersbridge.iEngage.core.events.users.RequestReadUserEvent;
+import com.eulersbridge.iEngage.core.services.UserService;
+import com.eulersbridge.iEngage.rest.domain.User;
 
+//@Controller
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
-	@Autowired UserRepository repo;
-	public UserController() {
-		// TODO Auto-generated constructor stub
+    @Autowired UserService userService;
+    
+	public UserController() 
+	{
+		if (LOG.isDebugEnabled()) LOG.debug("UserController()");
 	}
 
     private static Logger LOG = LoggerFactory.getLogger(UserController.class);
 
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping(method=RequestMethod.PUT,value="/api/user/{email}/{lastName}/{firstName}/{phoneNumber}/{personality}/{nationality}/{yearOfBirth}/{gender}/{password}")
+    @RequestMapping(method=RequestMethod.POST,value="/user")
     public @ResponseBody User addStudent(
-            @PathVariable String email, @PathVariable String lastName,@PathVariable String firstName,@PathVariable String phoneNumber, 
-            @PathVariable String personality, @PathVariable String nationality,@PathVariable String yearOfBirth,
-            @PathVariable String gender, @PathVariable String password) 
+    		@RequestBody User user) 
     {
-    	if (LOG.isInfoEnabled()) LOG.info(email+" attempting to save user. ");
-    	User student=new User(email,lastName,firstName,personality,nationality,yearOfBirth,gender,password);
-		User test = repo.save(student);
-		if (LOG.isDebugEnabled()) LOG.debug("test = "+test);
-		if (LOG.isDebugEnabled()) LOG.debug("Count = "+repo.count());
-		User result = repo.findOne(test.getNodeId());
+    	if (LOG.isInfoEnabled()) LOG.info(user.getEmail()+" attempting to save user. ");
+//		User test = repo.save(user);
+//		if (LOG.isDebugEnabled()) LOG.debug("test = "+test);
+//		if (LOG.isDebugEnabled()) LOG.debug("Count = "+repo.count());
+//		User result = repo.findOne(test.getNodeId());
+		User result = user;
     	return result;
     }
     
-    @RequestMapping(method=RequestMethod.GET,value="/api/user")
-    public @ResponseBody User findStudent(
-    		@RequestParam(value="email", required=true) String email) 
+    @RequestMapping(method=RequestMethod.GET,value="/user/{email}")
+//    @ResponseStatus(HttpStatus.OK);
+    public @ResponseBody ResponseEntity<User> findUser(@PathVariable String email) 
     {
     	if (LOG.isInfoEnabled()) LOG.info(email+" attempting to retrieve user. ");
-		Result <User> users = repo.findAll();
-		Iterator<User> iter=users.iterator();
-		User user=null;
-		while (iter.hasNext())
-		{
-			User res=iter.next();
-			if (res.getEmail().equals(email))
-				user=res;
-			if (LOG.isDebugEnabled()) LOG.debug("res = "+res);
-		}
-		if (LOG.isDebugEnabled()) LOG.debug("student = "+user);
-		if (LOG.isDebugEnabled()) LOG.debug("Count = "+repo.count());
-    	return user;
-    }
-	    
-	    
+    	ReadUserEvent userEvent=userService.requestReadUser(new RequestReadUserEvent(email));
+    	
+    	if (!userEvent.isEntityFound())
+    	{
+    		return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+    	}
+    	User restUser=User.fromRequestUser(userEvent.getReadUserDetails());
+    	return new ResponseEntity<User>(restUser,HttpStatus.OK);
+    }	    
 }
 
