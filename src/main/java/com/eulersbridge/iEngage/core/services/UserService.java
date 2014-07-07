eda180f364ea6f096845019f513aef8125e8d328
Greg Newitt <greg.newitt@eulersbridge.com>
2014-07-07 14:44:17 +1000

    Altered interface to reflect required methods.

--- src/main/java/com/eulersbridge/iEngage/core/services/UserService.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/UserService.java
@@ -1,11 +1,15 @@
 package com.eulersbridge.iEngage.core.services;
 
+import com.eulersbridge.iEngage.core.events.users.ReadUserEvent;
+import com.eulersbridge.iEngage.core.events.users.RequestReadUserEvent;
+import com.eulersbridge.iEngage.core.events.users.UserDetails;
 import com.eulersbridge.iEngage.database.domain.User;
 
 //All methods are guaranteed to return something, null will never be returned.
 public interface UserService 
 {
-	  public User signUpNewUser(User newUser, Long InstitutionId);
-	  public User findUserByEmail(String emailAddress);
+	  public User signUpNewUser(UserDetails newUser);
+	  public User findUser(String email);
+	  public ReadUserEvent requestReadUser(RequestReadUserEvent requestReadUserEvent);
 
 }
