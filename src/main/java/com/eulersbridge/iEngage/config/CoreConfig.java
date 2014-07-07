d3b7086d5864840fcfed42213f3f952dc5ff130b
Greg Newitt <greg.newitt@eulersbridge.com>
2014-07-07 14:34:18 +1000

    Initial commit of CoreConfig. createService (for userService) is in here And have put logging in here too.

---
+++ src/main/java/com/eulersbridge/iEngage/config/CoreConfig.java
@@ -0,0 +1,39 @@
+package com.eulersbridge.iEngage.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.eulersbridge.iEngage.core.services.UserEventHandler;
+import com.eulersbridge.iEngage.core.services.UserService;
+import com.eulersbridge.iEngage.database.repository.UserRepository;
+
+@Configuration
+public class CoreConfig 
+{
+	@Autowired
+	UserRepository repo;
+	
+    private static Logger LOG = LoggerFactory.getLogger(CoreConfig.class);
+
+	public CoreConfig()
+	{
+		if (LOG.isDebugEnabled()) LOG.debug("CoreConfig()");
+	}
+
+	@Bean
+	public UserService createService() 
+	{
+		if (LOG.isDebugEnabled()) LOG.debug("createService()");
+	    return new UserEventHandler(repo);
+	}
+
+/*	  @Bean
+	  public OrdersRepository createRepo() {
+	    return new OrdersMemoryRepository(new HashMap<UUID, Order>());
+	  }
+*/
+
+}
