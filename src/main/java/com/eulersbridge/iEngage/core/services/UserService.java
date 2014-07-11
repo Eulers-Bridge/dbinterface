57f8aacb220fba3fb9c53757ca38dd9161288dc0
Greg Newitt <greg.newitt@eulersbridge.com>
2014-07-11 15:23:30 +1000

    Added deleteUser and signUpNewUser with CreateUserEvent

--- src/main/java/com/eulersbridge/iEngage/core/services/UserService.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/UserService.java
@@ -1,15 +1,19 @@
 package com.eulersbridge.iEngage.core.services;
 
+import com.eulersbridge.iEngage.core.events.users.CreateUserEvent;
 import com.eulersbridge.iEngage.core.events.users.ReadUserEvent;
 import com.eulersbridge.iEngage.core.events.users.RequestReadUserEvent;
+import com.eulersbridge.iEngage.core.events.users.UserCreatedEvent;
 import com.eulersbridge.iEngage.core.events.users.UserDetails;
 import com.eulersbridge.iEngage.database.domain.User;
 
 //All methods are guaranteed to return something, null will never be returned.
 public interface UserService 
 {
-	  public User signUpNewUser(UserDetails newUser);
-	  public User findUser(String email);
-	  public ReadUserEvent requestReadUser(RequestReadUserEvent requestReadUserEvent);
+//	public User signUpNewUser(UserDetails newUser);
+	public User findUser(String email);
+	public ReadUserEvent requestReadUser(RequestReadUserEvent requestReadUserEvent);
+	public UserCreatedEvent signUpNewUser(CreateUserEvent createUserEvent);
+	public UserDeletedEvent deleteUser(DeleteUserEvent deleteUserEvent);
 
 }
