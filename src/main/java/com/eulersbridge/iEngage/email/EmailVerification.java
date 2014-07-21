ee57e120ef78610463bcca2beed929cc9ea5ab46
Greg Newitt <greg.newitt@eulersbridge.com>
2014-07-21 22:23:23 +1000

    Removed templating temporarily.

--- src/main/java/com/eulersbridge/iEngage/email/EmailVerification.java
+++ src/main/java/com/eulersbridge/iEngage/email/EmailVerification.java
@@ -11,11 +11,16 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.velocity.Template;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.exception.VelocityException;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.ui.velocity.VelocityEngineFactory;
@@ -30,7 +35,8 @@ public class EmailVerification extends Email implements Serializable
 
     private final String token;
     private final String tokenType;
-    private final String velocityModel;
+//    private final String velocityModel= "classpath:**/validateEmailBody.vm";
+    private final String velocityModel= "classpath:templates/validateEmailBody.vm";
 	@Autowired
     private VelocityEngine velocityEngine;
 
@@ -41,51 +47,18 @@ public class EmailVerification extends Email implements Serializable
     	super(user.getEmail(),user.getFirstName() + " " + user.getLastName(),"greg.newitt@eulersbridge.com","Email Verification Test");
         this.token = token.getToken();
         this.tokenType = token.getTokenType();
-        this.velocityModel = "/validateEmailBody.vm";
         VelocityEngineFactory vfact=new VelocityEngineFactory();
 		Properties velocityProperties=new Properties();
-		velocityProperties.setProperty("resource.loader", "class");
-		velocityProperties.setProperty("class.respource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+		velocityProperties.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+		velocityProperties.setProperty("class.resource.loader.class", ClasspathResourceLoader.class.getName());
         vfact.setVelocityProperties(velocityProperties);
-        VelocityConfigurer vcf=new VelocityConfigurer();
-        HashMap<String,Object> vCfMap=new HashMap<String,Object>();
-        vCfMap.put("runtime.log.invalid.reference", true);
-        vCfMap.put("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.Log4JLogChute");
-        vCfMap.put("runtime.log.logsystem.log4j.logger", "velocity");
-        vCfMap.put("input.encoding", "UTF-8");
-        vCfMap.put("output.encoding", "UTF-8");
-        vCfMap.put("directive.include.output.errormsg.start","");
-        vCfMap.put("directive.parse.max.depth","10");
-        vCfMap.put("directive.set.null.allowed",true);
-        vCfMap.put("velocimacro.library.autoreload",true);
-        vCfMap.put("velocimacro.permissions.allow.inline",true);
-        vCfMap.put("velocimacro.permissions.allow.inline.to.replace.global",false);
-        vCfMap.put("velocimacro.permissions.allow.inline.local.scope",false);
-        vCfMap.put("velocimacro.context.localscope","false");
-        vCfMap.put("runtime.interpolate.string.literals","true");
-        vCfMap.put("resource.manager.class","org.apache.velocity.runtime.resource.ResourceManagerImpl");
-        vCfMap.put("resource.manager.cache.class","org.apache.velocity.runtime.resource.ResourceCacheImpl");
-        vCfMap.put("resource.loader","webapp, class, ds");
-        vCfMap.put("class.resource.loader.description","Velocity Classpath Resource Loader");
-        vCfMap.put("class.resource.loader.class","org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-        vCfMap.put("webapp.resource.loader.class","org.apache.velocity.tools.view.WebappResourceLoader");
-        vCfMap.put("webapp.resource.loader.path","/WEB-INF/views/");
-        vCfMap.put("webapp.resource.loader.cache","false");
-        vCfMap.put("webapp.resource.loader.modificationCheckInterval","2");
-       
-        vcf.setVelocityPropertiesMap(vCfMap);
-/*        <property name="velocityPropertiesMap">
-                <entry key="ds.resource.loader.instance"><ref bean="templateLoader"/></entry>
-                <entry key="ds.resource.loader.resource.table"><value>templates</value></entry>
-                <entry key="ds.resource.loader.resource.keycolumn"><value>code</value></entry>
-                <entry key="ds.resource.loader.resource.templatecolumn"><value>content</value></entry>
-                <entry key="ds.resource.loader.resource.timestampcolumn"><value>updated</value></entry>
-                <entry key="ds.resource.loader.cache"><value>false</value></entry>
-*/        
         
         try {
 			this.velocityEngine = vfact.createVelocityEngine();
-//			this.velocityEngine = vcf.createVelocityEngine();
+			this.velocityEngine.init();
+			Template t;
+//			 t=this.velocityEngine.getTemplate("logback.xml");
+//			 t=this.velocityEngine.getTemplate("templates/validateEmailBody.vm");
 		} catch (VelocityException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -124,10 +97,10 @@ public class EmailVerification extends Email implements Serializable
     			hTemplateVariables.put("email", this);
     			if (LOG.isDebugEnabled()) LOG.debug("Velocity engine :"+velocityEngine);
     			if (LOG.isDebugEnabled())
-    			body = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, velocityModel,"UTF-8", hTemplateVariables);
-            
-            if(LOG.isDebugEnabled()) LOG.debug("body={}", body);
-            message.setText(body, true);
+//    			body = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, velocityModel,"UTF-8", hTemplateVariables);
+    			body="Dear "+getRecipientName()+", your token is "+getEncodedToken()+" your url is http://eulersbridge.com:8080/api/emailVerification/"+getRecipientEmailAddress()+"/"+getEncodedToken()+" Thankyou.";
+    			if(LOG.isDebugEnabled()) LOG.debug("body={}", body);
+    			message.setText(body, true);
 
            /*for(String resourceIdentifier: hTemplateVariables.keySet()) {
           	 //Inline resources are added to the mime message using the specified Content-ID. 
@@ -141,6 +114,10 @@ public class EmailVerification extends Email implements Serializable
     	};
     	
     	return preparator;
+    }
+    public String toString()
+    {
+    	return super.toString()+" token: "+token.toString();
     }
 
 }
