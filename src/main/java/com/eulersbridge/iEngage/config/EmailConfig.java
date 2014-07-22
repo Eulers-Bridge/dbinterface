7119b112ca304f21a976791c8597669af9b53db1
rdivad007 <rdivad007@gmail.com>
2014-07-22 15:34:10 -0500

    Set the email.properties file to be read from the email package

--- src/main/java/com/eulersbridge/iEngage/config/EmailConfig.java
+++ src/main/java/com/eulersbridge/iEngage/config/EmailConfig.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -15,6 +16,7 @@ import org.springframework.ui.velocity.VelocityEngineFactoryBean;
 import com.eulersbridge.iEngage.core.services.EmailEventHandler;
 import com.eulersbridge.iEngage.core.services.EmailService;
 
+@PropertySource("classpath:/com/eulersbridge/iEngage/email/email.properties")
 @Configuration
 public class EmailConfig 
 {
