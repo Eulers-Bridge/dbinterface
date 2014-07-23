36dbe25a2498446e495f5f7c1c4324331b7c6a20
Greg Newitt <greg.newitt@eulersbridge.com>
2014-07-23 23:25:08 +1000

    Added toCountryDetails and fromCountryDetails methods.

--- src/main/java/com/eulersbridge/iEngage/database/domain/Country.java
+++ src/main/java/com/eulersbridge/iEngage/database/domain/Country.java
@@ -5,9 +5,12 @@ import java.util.Set;
 import org.neo4j.graphdb.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.RelatedTo;
+
+import com.eulersbridge.iEngage.core.events.countrys.CountryDetails;
 
 @NodeEntity
 public class Country 
@@ -37,5 +40,54 @@ public class Country
 	
 	public void setCountryName(String countryName) {
 		this.countryName = countryName;
+	}
+	
+	public String toString()
+	{
+		StringBuffer buff=new StringBuffer("[ nodeId = ");
+		String retValue;
+		buff.append(getNodeId());
+		buff.append(", name = ");
+		buff.append(getCountryName());
+		buff.append(" ]");
+		retValue=buff.toString();
+		if (LOG.isDebugEnabled()) LOG.debug("toString() = "+retValue);
+		return retValue;
+	}
+	
+	public CountryDetails toCountryDetails() 
+	{
+	    if (LOG.isTraceEnabled()) LOG.trace("toCountryDetails()");
+	    
+	    CountryDetails details = new CountryDetails(getNodeId());
+	    if (LOG.isTraceEnabled()) LOG.trace("country "+this);
+
+	    BeanUtils.copyProperties(this, details);
+	    if (LOG.isTraceEnabled()) LOG.trace("countryDetails "+details);
+
+	    return details;
+	}
+
+	  public static Country fromCountryDetails(CountryDetails countryDetails) 
+	  {
+		    if (LOG.isTraceEnabled()) LOG.trace("fromCountryDetails()");
+
+		    if (LOG.isTraceEnabled()) LOG.trace("countryDetails "+countryDetails);
+		    Country country = new Country(countryDetails.getCountryName());
+		    country.nodeId=countryDetails.getCountryId();
+		    if (LOG.isTraceEnabled()) LOG.trace("country "+country);
+
+		    return country;
+		  }
+	  
+	  public boolean equals(Country country2)
+	  {
+		  if ((nodeId!=null)&&(nodeId.equals(country2.nodeId))) return true;
+		  else return false;
+	  }
+
+	public void setNodeId(Long id) 
+	{
+		this.nodeId=id;
 	}
 }
