f54b63233bb9a3a98d55628e437d1075a2caae6b
Greg Newitt <greg.newitt@eulersbridge.com>
2014-08-05 09:44:18 +1000

    Removed unnecessary import

--- src/main/java/com/eulersbridge/iEngage/core/services/UserEventHandler.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/UserEventHandler.java
@@ -2,7 +2,6 @@ package com.eulersbridge.iEngage.core.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eulersbridge.iEngage.core.events.users.CreateUserEvent;
