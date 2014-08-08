6a88efe455e9323422d5c741db4264ae65953697
Greg Newitt <greg.newitt@eulersbridge.com>
2014-08-08 16:19:26 +1000

    Added verifyUser method.

--- src/main/java/com/eulersbridge/iEngage/database/repository/UserRepository.java
+++ src/main/java/com/eulersbridge/iEngage/database/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.eulersbridge.iEngage.database.repository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.eulersbridge.iEngage.database.domain.User;
 
@@ -11,4 +13,8 @@ public interface UserRepository extends GraphRepository<User>
     static Logger LOG = LoggerFactory.getLogger(UserRepository.class);
 	
  	User findByEmail(String email);
+ 	
+	@Query("MATCH (u:`User`)-[r:verifiedBy]-(v:`VerificationToken`) where ID(u)={userId} AND ID(v)={tokenId} set u.accountVerified={isVerified} set v.verified={isVerified} ")
+	void verifyUser(@Param("userId") Long userId, @Param("tokenId") Long tokenId, boolean isVerified);
+
 }
