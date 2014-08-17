9718dce7b8f46380c514401ce587eb786cc68814
Greg Newitt <greg.newitt@eulersbridge.com>
2014-08-17 11:56:33 +1000

    Removed unused import.

--- src/main/java/com/eulersbridge/iEngage/Application.java
+++ src/main/java/com/eulersbridge/iEngage/Application.java
@@ -7,7 +7,6 @@ import javax.annotation.Resource;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
