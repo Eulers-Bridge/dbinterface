a8a0f793b7f1ba236a3baeb4b754ce76f8195c56
Greg Newitt <greg.newitt@eulersbridge.com>
2014-08-19 23:10:44 +1000

    Modified general info controller so it is autowired.

--- src/main/java/com/eulersbridge/iEngage/rest/controller/GeneralInfoController.java
+++ src/main/java/com/eulersbridge/iEngage/rest/controller/GeneralInfoController.java
@@ -1,11 +1,8 @@
 package com.eulersbridge.iEngage.rest.controller;
 
-//import java.util.concurrent.atomic.AtomicLong;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-//import org.springframework.security.access.annotation.Secured;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +15,14 @@ import com.eulersbridge.iEngage.rest.domain.Country;
 public class GeneralInfoController {
 
 	    private static Logger LOG = LoggerFactory.getLogger(GeneralInfoController.class);
-
+@Autowired	    CountriesFactory countryFactory;
 //	    @Secured({ "ROLE_USER" })
 	    @RequestMapping("/general-info")
 	    public @ResponseBody Country[] generalInfo()
 	    {
 	    	if (LOG.isInfoEnabled()) LOG.info("general info called. ");
-	    	ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("application-context.xml");
-	    	CountriesFactory cf=(CountriesFactory)ctx.getBean("countryFactory");
 	    	
-	    	Country[] retValue=cf.getCountries();
-	    	ctx.close();
+	    	Country[] retValue=countryFactory.getCountries();
 	    	return retValue;
 	    }
 	    
