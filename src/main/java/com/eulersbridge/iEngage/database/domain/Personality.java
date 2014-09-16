ebfe6d9706deb3ba2a029bb6a1a6f663c6306274
Greg Newitt <greg.newitt@eulersbridge.com>
2014-09-16 22:32:19 +1000

    Added Personality core domain class.

--- src/main/java/com/eulersbridge/iEngage/database/domain/Personality.java
+++ src/main/java/com/eulersbridge/iEngage/database/domain/Personality.java
@@ -1,7 +1,12 @@
 package com.eulersbridge.iEngage.database.domain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.NodeEntity;
+
+import com.eulersbridge.iEngage.core.events.users.PersonalityDetails;
 
 @NodeEntity
 public class Personality 
@@ -14,6 +19,8 @@ public class Personality
 	private float emotionalStability;
 	private float openess;
 
+    private static Logger LOG = LoggerFactory.getLogger(Personality.class);
+    
 	/**
 	 * @param extroversion
 	 * @param agreeableness
@@ -70,5 +77,49 @@ public class Personality
 	public Long getNodeId()
 	{
 		return nodeId;
+	}
+
+	public void setNodeId(Long nodeId) 
+	{
+		this.nodeId=nodeId;
+		
+	}
+	
+	public PersonalityDetails toPersonalityDetails()
+	{
+	    if (LOG.isTraceEnabled()) LOG.trace("toPersonalityDetails()");
+	    
+	    PersonalityDetails details = new PersonalityDetails();
+	    if (LOG.isTraceEnabled()) LOG.trace("personality "+this);
+
+	    BeanUtils.copyProperties(this, details);
+	    details.setPersonalityId(getNodeId());
+	    if (LOG.isTraceEnabled()) LOG.trace("personalityDetails "+details);
+
+	    return details;
+	}
+	
+	public static Personality fromPersonalityDetails(PersonalityDetails dets)
+	{
+	    if (LOG.isTraceEnabled()) LOG.trace("fromUserDetails()");
+
+	    if (LOG.isTraceEnabled()) LOG.trace("personalityDetails "+dets);
+	    Personality personality = new Personality(dets.getExtroversion(),dets.getAgreeableness(), dets.getConscientiousness(), dets.getEmotionalStability(), dets.getOpeness());
+	    personality.setNodeId(dets.getPersonalityId());
+	    if (LOG.isTraceEnabled()) LOG.trace("personality "+personality);
+
+	    return personality;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Personality [nodeId=" + nodeId + ", extroversion="
+				+ extroversion + ", agreeableness=" + agreeableness
+				+ ", conscientiousness=" + conscientiousness
+				+ ", emotionalStability=" + emotionalStability + ", openess="
+				+ openess + "]";
 	}
 }
