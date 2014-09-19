f625113219c9e05a59bf6dcab05a14639f819a03
Greg Newitt <greg.newitt@eulersbridge.com>
2014-09-19 15:28:22 +1000

    Linking created personality to user now.

--- src/main/java/com/eulersbridge/iEngage/database/repository/UserRepository.java
+++ src/main/java/com/eulersbridge/iEngage/database/repository/UserRepository.java
@@ -18,6 +18,6 @@ public interface UserRepository extends GraphRepository<User>
 	@Query("MATCH (u:`User`)-[r:verifiedBy]-(v:`VerificationToken`) where ID(u)={userId} AND ID(v)={tokenId} set u.accountVerified={isVerified} set v.verified={isVerified} ")
 	void verifyUser(@Param("userId") Long userId, @Param("tokenId") Long tokenId, @Param("isVerified") boolean isVerified);
 
-	@Query("MATCH (u:`User`) where ID(u)={userId} create (p:Personality{personality}) create (u)-[r:hasPersonality]->(p) return p;")
-	Personality addPersonality(@Param("userId") Long userId,@Param("personality") Personality personality);
+	@Query("MATCH (u:`User`) where ID(u)={userId} MATCH (p:`Personality`) where ID(p)={personalityId} create (u)-[r:hasPersonality]->(p) return p;")
+	Personality addPersonality(@Param("userId") Long userId,@Param("personalityId") Long personalityId);
 }
