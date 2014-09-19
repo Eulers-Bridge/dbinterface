9fbbb0437ae1ea010b641a701768647e95772379
Greg Newitt <greg.newitt@eulersbridge.com>
2014-09-19 13:06:41 +1000

    Added addPersonality

--- src/main/java/com/eulersbridge/iEngage/database/repository/UserRepository.java
+++ src/main/java/com/eulersbridge/iEngage/database/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.repository.query.Param;
 
+import com.eulersbridge.iEngage.database.domain.Personality;
 import com.eulersbridge.iEngage.database.domain.User;
 
 public interface UserRepository extends GraphRepository<User> 
@@ -17,4 +18,6 @@ public interface UserRepository extends GraphRepository<User>
 	@Query("MATCH (u:`User`)-[r:verifiedBy]-(v:`VerificationToken`) where ID(u)={userId} AND ID(v)={tokenId} set u.accountVerified={isVerified} set v.verified={isVerified} ")
 	void verifyUser(@Param("userId") Long userId, @Param("tokenId") Long tokenId, @Param("isVerified") boolean isVerified);
 
+	@Query("MATCH (u:`User`) where ID(u)={userId} create (p:Personality{personality}) create (u)-[r:hasPersonality]->(p) return p;")
+	Personality addPersonality(@Param("userId") Long userId,@Param("personality") Personality personality);
 }
