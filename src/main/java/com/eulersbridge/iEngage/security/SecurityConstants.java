68c2c1d4aa0eb7507e552ab2aee3be0f07af634b
Greg Newitt <greg.newitt@eulersbridge.com>
2014-09-23 13:00:46 +1000

    Added new roles.

--- src/main/java/com/eulersbridge/iEngage/security/SecurityConstants.java
+++ src/main/java/com/eulersbridge/iEngage/security/SecurityConstants.java
@@ -4,4 +4,9 @@ public class SecurityConstants
 {
 	public static final String USER_ROLE = "ROLE_USER";
 	public static final String ADMIN_ROLE = "ROLE_ADMIN";
+	public static final String CONTENT_MANAGER_ROLE = "ROLE_CONTENT_MANAGER";
+	public static final String RETURNING_OFFICER_ROLE = "ROLE_RETURNING_OFFICER";
+	
+	public static final String REALM_NAME = "isegoria";
+	public static final String DIGEST_KEY = "eulersBridge";
 }
