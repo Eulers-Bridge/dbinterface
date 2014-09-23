3dd26d470e0431961c10ef190e446dacddf671a8
Greg Newitt <greg.newitt@eulersbridge.com>
2014-09-23 23:24:19 +1000

    Implement method security for NewsService

--- src/main/java/com/eulersbridge/iEngage/core/services/NewsService.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/NewsService.java
@@ -1,6 +1,7 @@
 package com.eulersbridge.iEngage.core.services;
 
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.eulersbridge.iEngage.core.events.newsArticles.CreateNewsArticleEvent;
 import com.eulersbridge.iEngage.core.events.newsArticles.DeleteNewsArticleEvent;
@@ -19,11 +20,18 @@ import com.eulersbridge.iEngage.core.events.newsArticles.UpdateNewsArticleEvent;
 
 public interface NewsService 
 {
+	@PreAuthorize("hasAnyRole('ROLE_CONTENT_MANAGER','ROLE_ADMIN')")
 	public NewsArticleCreatedEvent createNewsArticle(CreateNewsArticleEvent createNewsArticleEvent);
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public ReadNewsArticleEvent requestReadNewsArticle(RequestReadNewsArticleEvent requestReadNewsArticleEvent);
+	@PreAuthorize("hasAnyRole('ROLE_CONTENT_MANAGER','ROLE_ADMIN')")
 	public NewsArticleUpdatedEvent updateNewsArticle(UpdateNewsArticleEvent updateNewsArticleEvent);
+	@PreAuthorize("hasAnyRole('ROLE_CONTENT_MANAGER','ROLE_ADMIN')")
 	public NewsArticleDeletedEvent deleteNewsArticle(DeleteNewsArticleEvent deleteNewsArticleEvent);
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public NewsArticlesReadEvent readNewsArticles(ReadNewsArticlesEvent readNewsArticlesEvent,Direction sortDirection, int i, int j);
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public NewsArticleLikedEvent likeNewsArticle(LikeNewsArticleEvent likeNewsArticlesEvent);
+	@PreAuthorize("hasRole('ROLE_USER')")
 	public NewsArticleUnlikedEvent unlikeNewsArticle(UnlikeNewsArticleEvent unlikeNewsArticleEvent); 
 }
