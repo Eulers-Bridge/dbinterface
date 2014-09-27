3808425f63490536f20475e12490c13c9d9b6dc9
darcular <yikaig@student.unimelb.edu.au>
2014-09-27 14:49:19 +1000

    Implement method security for ElectionService

--- src/main/java/com/eulersbridge/iEngage/core/services/ElectionService.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/ElectionService.java
@@ -1,15 +1,28 @@
 package com.eulersbridge.iEngage.core.services;
 
 import com.eulersbridge.iEngage.core.events.Elections.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Created by darcular on 4/09/14.
  */
 public interface ElectionService {
+
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ReadElectionEvent requestReadElection(RequestReadElectionEvent requestReadElectionEvent);
+
+    @PreAuthorize("hasAnyRole('ROLE_CONTENT_MANAGER','ROLE_ADMIN')")
     public ElectionCreatedEvent createElection(CreateElectionEvent createElectionEvent);
+
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ReadElectionEvent readPreviousElection(RequestReadElectionEvent requestReadElectionEvent);
+
+    @PreAuthorize("hasRole('ROLE_USER')")
     public ReadElectionEvent readNextElection(RequestReadElectionEvent requestReadElectionEvent);
+
+    @PreAuthorize("hasAnyRole('ROLE_CONTENT_MANAGER','ROLE_ADMIN')")
     public ElectionDeletedEvent deleteElection(DeleteElectionEvent deleteElectionEvent);
+
+    @PreAuthorize("hasAnyRole('ROLE_CONTENT_MANAGER','ROLE_ADMIN')")
     public ElectionUpdatedEvent updateElection(UpdateElectionEvent updateElectionEvent);
 }
