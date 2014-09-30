6a3d4475e6c3a466eda9f5b332702455a325ef25
Greg Newitt <greg.newitt@eulersbridge.com>
2014-09-30 16:49:19 +1000

    Update login controller

--- src/main/java/com/eulersbridge/iEngage/rest/controller/LoginController.java
+++ src/main/java/com/eulersbridge/iEngage/rest/controller/LoginController.java
@@ -1,8 +1,14 @@
 package com.eulersbridge.iEngage.rest.controller;
 
+import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.catalina.authenticator.AuthenticatorBase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,37 +20,35 @@ import org.slf4j.LoggerFactory;
 
 import com.eulersbridge.iEngage.core.domain.Login;
 import com.eulersbridge.iEngage.core.domain.Logout;
+import com.eulersbridge.iEngage.core.events.users.LogInUserEvent;
+import com.eulersbridge.iEngage.core.events.users.UserLogInEvent;
+import com.eulersbridge.iEngage.core.services.UserService;
 import com.eulersbridge.iEngage.rest.domain.Response;
 import com.eulersbridge.iEngage.rest.domain.SignUp;
 
 @RestController
 @RequestMapping(ControllerConstants.API_PREFIX)
-public class LoginController {
+public class LoginController 
+{
+    @Autowired UserService userService;
 
     private static Logger LOG = LoggerFactory.getLogger(LoginController.class);
 
     private final AtomicLong counter = new AtomicLong();
 
-/*    
-//    @Secured({ "ROLE_USER" })
-    @RequestMapping("/general-info")
-    public @ResponseBody Country[] generalInfo()
-    {
-    	if (LOG.isInfoEnabled()) LOG.info("general info called. ");
-    	ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("application-context.xml");
-    	CountriesFactory cf=(CountriesFactory)ctx.getBean("countryFactory");
-    	
-    	Country[] retValue=cf.getCountries();
-    	ctx.close();
-    	return retValue;
-    }
-*/    
-    @Secured({ "ROLE_USER" })
     @RequestMapping(value=ControllerConstants.LOGIN_LABEL,method=RequestMethod.GET)
-  	public @ResponseBody Response login( @RequestParam(value="username", required=true) String username,
-            @RequestParam(value="password", required=true) String password) 
+  	public @ResponseBody Response login() 
     {
+    	SecurityContext context=SecurityContextHolder.getContext();
+    	Authentication authentication=context.getAuthentication();
+    	String username=authentication.getName();
+    	String password="*******";
+    	Collection<? extends GrantedAuthority> roles = authentication.getAuthorities();
+    	if (authentication.getCredentials()!=null) password=authentication.getCredentials().toString();
     	if (LOG.isInfoEnabled()) LOG.info(username+" attempting to login. ");
+    	if (LOG.isDebugEnabled()) LOG.debug("auth - "+authentication+" username - "+username+" credentails - "+password+" roles - "+roles);
+    	LogInUserEvent evt=new LogInUserEvent(username,roles);
+    	UserLogInEvent retEvt=userService.login(evt);
     	Login login=new Login(counter.incrementAndGet(),username,password);
     	Response response=login.process();
 		return response;
@@ -56,26 +60,8 @@ public class LoginController {
     	String username=SecurityContextHolder.getContext().getAuthentication().getName();
     	if (LOG.isInfoEnabled()) LOG.info(username+" attempting to logout. ");
     	Logout logout=new Logout(counter.incrementAndGet(),username);
+    	SecurityContextHolder.clearContext();
     	Response response=logout.process();
         return response;
     }
-    
-    @RequestMapping(value="/signup",method=RequestMethod.POST)
-    public @ResponseBody Response signup(
-            @RequestParam(value="email", required=true) String email,
-            @RequestParam(value="fullName", required=true) String fullName,
-            @RequestParam(value="password", required=true) String password,
-            @RequestParam(value="countryID", required=true) String countryId,
-            @RequestParam(value="universityID", required=true) String universityId
-            ) 
-    {
-    	if (LOG.isInfoEnabled()) LOG.info("attempting to signup "+email);
-    	SignUp signup=new SignUp(email,fullName,password,countryId,universityId);
-    	
-    	Response response=signup.process();
-    	
-        return response;
-    }
-    
-    
 }
