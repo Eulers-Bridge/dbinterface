e81bf8a43c43ca59c61a361c7bbccad64ffd4f19
Greg Newitt <greg.newitt@eulersbridge.com>
2014-09-30 11:47:07 +1000

    Add constants file into Controller package.

---
+++ src/main/java/com/eulersbridge/iEngage/rest/controller/ControllerConstants.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.eulersbridge.iEngage.rest.controller;
+
+/**
+ * @author Greg Newitt
+ *
+ */
+public class ControllerConstants 
+{
+	public static final String API_PREFIX="/api";
+	public static final String COUNTRY_LABEL = "/country";
+	public static final String COUNTRYS_LABEL = "/countrys";
+	public static final String ELECTION_LABEL = "/election";
+	public static final String EMAIL_VERIFICATION_LABEL = "/emailVerification";
+	public static final String INSTITUTION_LABEL = "/institution";
+	public static final String INSTITUTIONS_LABEL = "/institutions";
+	public static final String LOGIN_LABEL = "/login";
+	public static final String LOGOUT_LABEL = "/logout";
+	public static final String NEWS_ARTICLE_LABEL = "/newsArticle";
+	public static final String NEWS_ARTICLES_LABEL = "/newsArticles";
+	public static final String POLL_LABEL = "/poll";
+	public static final String SIGNUP_LABEL = "/signUp";
+	public static final String USER_LABEL = "/user";
+
+}
