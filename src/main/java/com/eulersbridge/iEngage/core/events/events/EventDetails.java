822540eb5d986e6342126794ebf63e579d1ee86b
darcular <yikaig@student.unimelb.edu.au>
2014-10-02 13:04:05 +1000

    Add properties to Event

--- src/main/java/com/eulersbridge/iEngage/core/events/events/EventDetails.java
+++ src/main/java/com/eulersbridge/iEngage/core/events/events/EventDetails.java
@@ -10,7 +10,16 @@ import org.slf4j.LoggerFactory;
 
 public class EventDetails {
     private Long eventId;
-    //TODO Add properties
+    private String name;
+    private String location;
+    private Long date;
+    private String description;
+    private String picture[];
+    private String volunteerPositions[];
+    private Long created;
+    private String organizer;
+    private String organizerEmail;
+    private Long modified;
 
     private static Logger LOG = LoggerFactory.getLogger(EventDetails.class);
 
@@ -21,8 +30,26 @@ public class EventDetails {
         StringBuffer buff = new StringBuffer("[ id = ");
         String retValue;
         buff.append(getEventId());
-        //TODO Add other properties
-
+        buff.append(", name = ");
+        buff.append(getName());
+        buff.append(", location = ");
+        buff.append(getLocation());
+        buff.append(", date = ");
+        buff.append(getDate());
+        buff.append(", description = ");
+        buff.append(getDescription());
+        buff.append(", picture = ");
+        buff.append(getPicture());
+        buff.append(", volunteerPositions = ");
+        buff.append(getVolunteerPositions());
+        buff.append(", created = ");
+        buff.append(getCreated());
+        buff.append(", organizer = ");
+        buff.append(getOrganizer());
+        buff.append(", organizerEmail = ");
+        buff.append(getOrganizerEmail());
+        buff.append(", modified = ");
+        buff.append(getModified());
         buff.append(" ]");
         retValue = buff.toString();
         if (LOG.isDebugEnabled()) LOG.debug("toString() = "+retValue);
@@ -48,5 +75,85 @@ public class EventDetails {
 
     public void setEventId(Long eventId) {
         this.eventId = eventId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String[] picture) {
+        this.picture = picture;
+    }
+
+    public String[] getVolunteerPositions() {
+        return volunteerPositions;
+    }
+
+    public void setVolunteerPositions(String[] volunteerPositions) {
+        this.volunteerPositions = volunteerPositions;
+    }
+
+    public Long getCreated() {
+        return created;
+    }
+
+    public void setCreated(Long created) {
+        this.created = created;
+    }
+
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    public String getOrganizerEmail() {
+        return organizerEmail;
+    }
+
+    public void setOrganizerEmail(String organizerEmail) {
+        this.organizerEmail = organizerEmail;
+    }
+
+    public Long getModified() {
+        return modified;
+    }
+
+    public void setModified(Long modified) {
+        this.modified = modified;
     }
 }
--- src/main/java/com/eulersbridge/iEngage/rest/domain/Event.java
+++ src/main/java/com/eulersbridge/iEngage/rest/domain/Event.java
@@ -14,7 +14,16 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 public class Event extends ResourceSupport{
     private Long eventId;
-    //TODO Add other properties
+    private String name;
+    private String location;
+    private Long date;
+    private String description;
+    private String picture[];
+    private String volunteerPositions[];
+    private Long created;
+    private String organizer;
+    private String organizerEmail;
+    private Long modified;
 
     private static Logger LOG = LoggerFactory.getLogger(Event.class);
 
@@ -26,7 +35,16 @@ public class Event extends ResourceSupport{
         String name = simpleName.substring(0, 1).toLowerCase()+simpleName.substring(1);
 
         event.setEventId(eventDetails.getEventId());
-        //TODO Add other properties
+        event.setName(eventDetails.getName());
+        event.setLocation(eventDetails.getLocation());
+        event.setDate(eventDetails.getDate());
+        event.setDescription(eventDetails.getDescription());
+        event.setPicture(eventDetails.getPicture());
+        event.setVolunteerPositions(eventDetails.getVolunteerPositions());
+        event.setCreated(eventDetails.getCreated());
+        event.setOrganizer(eventDetails.getOrganizer());
+        event.setOrganizerEmail(eventDetails.getOrganizerEmail());
+        event.setModified(eventDetails.getModified());
 
         event.add(linkTo(EventController.class).slash(name).slash(event.eventId).withSelfRel());
         event.add(linkTo(EventController.class).slash(name).slash(event.eventId).slash("previous").withRel("Previous"));
@@ -36,8 +54,18 @@ public class Event extends ResourceSupport{
 
     public EventDetails toEventDetails(){
         EventDetails eventDetails = new EventDetails();
+
         eventDetails.setEventId(this.getEventId());
-        //TODO Add other properties
+        eventDetails.setName(getName());
+        eventDetails.setLocation(getLocation());
+        eventDetails.setDate(getDate());
+        eventDetails.setDescription(getDescription());
+        eventDetails.setPicture(getPicture());
+        eventDetails.setVolunteerPositions(getVolunteerPositions());
+        eventDetails.setCreated(getCreated());
+        eventDetails.setOrganizer(getOrganizer());
+        eventDetails.setOrganizerEmail(getOrganizerEmail());
+        eventDetails.setModified(getModified());
 
         return eventDetails;
     }
@@ -50,4 +78,83 @@ public class Event extends ResourceSupport{
         this.eventId = eventId;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String[] picture) {
+        this.picture = picture;
+    }
+
+    public String[] getVolunteerPositions() {
+        return volunteerPositions;
+    }
+
+    public void setVolunteerPositions(String[] volunteerPositions) {
+        this.volunteerPositions = volunteerPositions;
+    }
+
+    public Long getCreated() {
+        return created;
+    }
+
+    public void setCreated(Long created) {
+        this.created = created;
+    }
+
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    public String getOrganizerEmail() {
+        return organizerEmail;
+    }
+
+    public void setOrganizerEmail(String organizerEmail) {
+        this.organizerEmail = organizerEmail;
+    }
+
+    public Long getModified() {
+        return modified;
+    }
+
+    public void setModified(Long modified) {
+        this.modified = modified;
+    }
 }
