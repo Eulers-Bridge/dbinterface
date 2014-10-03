51322ab81823483876854d9d103582e0809e71be
Greg Newitt <greg.newitt@eulersbridge.com>
2014-10-03 23:08:47 +1000

    Added logging.

--- src/main/java/com/eulersbridge/iEngage/core/events/forumQuestions/ForumQuestionCreatedEvent.java
+++ src/main/java/com/eulersbridge/iEngage/core/events/forumQuestions/ForumQuestionCreatedEvent.java
@@ -1,6 +1,7 @@
 package com.eulersbridge.iEngage.core.events.forumQuestions;
 
 import com.eulersbridge.iEngage.core.events.CreatedEvent;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,16 +9,22 @@ import org.slf4j.LoggerFactory;
  * @author Yikai Gong
  */
 
-public class ForumQuestionCreatedEvent extends CreatedEvent {
+public class ForumQuestionCreatedEvent extends CreatedEvent 
+{
     private Long forumQuestionId;
     private ForumQuestionDetails forumQuestionDetails;
 
-    public ForumQuestionCreatedEvent(Long forumQuestionId, ForumQuestionDetails forumQuestionDetails) {
+    private static Logger LOG = LoggerFactory.getLogger(ForumQuestionCreatedEvent.class);
+    
+    public ForumQuestionCreatedEvent(Long forumQuestionId, ForumQuestionDetails forumQuestionDetails) 
+    {
+    	this(forumQuestionId);
         this.forumQuestionDetails = forumQuestionDetails;
-        this.forumQuestionId = forumQuestionId;
     }
 
-    public ForumQuestionCreatedEvent(Long forumQuestionId) {
+    public ForumQuestionCreatedEvent(Long forumQuestionId) 
+    {
+    	if (LOG.isDebugEnabled()) LOG.debug("constructor()");
         this.forumQuestionId = forumQuestionId;
     }
 
