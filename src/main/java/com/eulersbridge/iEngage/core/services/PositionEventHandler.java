f14c053334ad382ca8b7413910168f09560237a1
darcular <yikaig@student.unimelb.edu.au>
2014-10-08 17:43:15 +1100

    Add CRUD handler for Position

--- src/main/java/com/eulersbridge/iEngage/core/services/PositionEventHandler.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/PositionEventHandler.java
@@ -1,6 +1,7 @@
 package com.eulersbridge.iEngage.core.services;
 
 import com.eulersbridge.iEngage.core.events.positions.*;
+import com.eulersbridge.iEngage.database.domain.Position;
 import com.eulersbridge.iEngage.database.repository.PositionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,21 +21,57 @@ public class PositionEventHandler implements PositionService{
 
     @Override
     public PositionCreatedEvent createPosition(CreatePositionEvent createPositionEvent) {
-        return null;
+        PositionDetails positionDetails = createPositionEvent.getPositionDetails();
+        Position position = Position.fromPositionDetails(positionDetails);
+        Position result = positionRepository.save(position);
+        PositionCreatedEvent positionCreatedEvent = new PositionCreatedEvent(result.getPositionId(), result.toPositionDetails());
+        return positionCreatedEvent;
     }
 
     @Override
     public ReadPositionEvent requestReadPosition(RequestReadPositionEvent requestReadPositionEvent) {
-        return null;
+        Position position = positionRepository.findOne(requestReadPositionEvent.getPositionId());
+        ReadPositionEvent readPositionEvent;
+        if(position != null){
+            readPositionEvent = new ReadPositionEvent(requestReadPositionEvent.getPositionId(), position.toPositionDetails());
+        }
+        else{
+            readPositionEvent = ReadPositionEvent.notFound(requestReadPositionEvent.getPositionId());
+        }
+        return readPositionEvent;
     }
 
     @Override
     public PositionUpdatedEvent updatePosition(UpdatePositionEvent updatePositionEvent) {
-        return null;
+        PositionDetails positionDetails = updatePositionEvent.getPositionDetails();
+        Position position = Position.fromPositionDetails(positionDetails);
+        Long positionId = positionDetails.getPositionId();
+        if(LOG.isDebugEnabled()) LOG.debug("positionId is " + positionId);
+        Position positionOld = positionRepository.findOne(positionId);
+        if(positionOld == null){
+            if(LOG.isDebugEnabled()) LOG.debug("position entity not found " + positionId);
+            return PositionUpdatedEvent.notFound(positionId);
+        }
+        else{
+            Position result = positionRepository.save(position);
+            if(LOG.isDebugEnabled()) LOG.debug("updated successfully" + result.getPositionId());
+            return new PositionUpdatedEvent(result.getPositionId(), result.toPositionDetails());
+        }
     }
 
     @Override
     public PositionDeletedEvent deletePosition(DeletePositionEvent deletePositionEvent) {
-        return null;
+        if (LOG.isDebugEnabled()) LOG.debug("Entered deletePositionEvent= "+deletePositionEvent);
+        Long positionId = deletePositionEvent.getPositionId();
+        if (LOG.isDebugEnabled()) LOG.debug("deletePosition("+positionId+")");
+        Position position = positionRepository.findOne(positionId);
+        if(position == null){
+            return PositionDeletedEvent.notFound(positionId);
+        }
+        else{
+            positionRepository.delete(positionId);
+            PositionDeletedEvent positionDeletedEvent = new PositionDeletedEvent(positionId);
+            return positionDeletedEvent;
+        }
     }
 }
