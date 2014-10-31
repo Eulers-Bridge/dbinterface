a738baf8f3437810f273a088e7c26c0b363f9713
Greg Newitt <greg.newitt@eulersbridge.com>
2014-10-31 21:32:01 +1100

    Rest Object for NewsArticles

---
+++ src/main/java/com/eulersbridge/iEngage/rest/domain/NewsArticles.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package com.eulersbridge.iEngage.rest.domain;
+
+import java.util.Iterator;
+
+import org.springframework.hateoas.ResourceSupport;
+
+/**
+ * @author Greg Newitt
+ *
+ */
+public class NewsArticles extends ResourceSupport
+{
+	Long totalArticles;
+	Integer totalPages;
+	Iterator<NewsArticle> articles;
+
+	/**
+	 * @return the totalArticles
+	 */
+	public Long getTotalArticles() {
+		return totalArticles;
+	}
+
+	/**
+	 * @param totalArticles the totalArticles to set
+	 */
+	public void setTotalArticles(Long totalArticles) {
+		this.totalArticles = totalArticles;
+	}
+
+	/**
+	 * @return the totalPages
+	 */
+	public Integer getTotalPages() {
+		return totalPages;
+	}
+
+	/**
+	 * @param totalPages the totalPages to set
+	 */
+	public void setTotalPages(Integer totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	/**
+	 * @return the articles
+	 */
+	public Iterator<NewsArticle> getArticles() {
+		return articles;
+	}
+
+	/**
+	 * @param articles the articles to set
+	 */
+	public void setArticles(Iterator<NewsArticle> articles) {
+		this.articles = articles;
+	}
+
+	public static  NewsArticles fromArticlesIterator(Iterator<NewsArticle> iter, Long totalArticles, Integer totalPages)
+	{
+	    NewsArticles news = new NewsArticles();
+//	    String simpleName=NewsArticles.class.getSimpleName();
+//	    String name=simpleName.substring(0, 1).toLowerCase()+simpleName.substring(1);
+
+	    news.totalArticles= totalArticles;
+	    news.totalPages = totalPages;
+	    news.articles = iter;
+	    
+	    //TODOCUMENT.  Adding the library, the above extends ResourceSupport and
+	    //this section is all that is actually needed in our model to add hateoas support.
+
+	    //Much of the rest of the framework is helping deal with the blending of domains that happens in many spring apps
+	    //We have explicitly avoided that.
+	    // {!begin selfRel}
+//	    news.add(linkTo(NewsController.class).slash(name).slash(news.articleId).withSelfRel());
+	    // {!end selfRel}
+
+	    return news;
+	}
+
+}
