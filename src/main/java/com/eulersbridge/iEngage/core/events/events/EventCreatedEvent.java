09ad7ff44366ec276f46c2909f7aa4e89abddd7b
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-04 12:18:27 +1100

    Add institutionFound var.

--- src/main/java/com/eulersbridge/iEngage/core/events/events/EventCreatedEvent.java
+++ src/main/java/com/eulersbridge/iEngage/core/events/events/EventCreatedEvent.java
@@ -9,29 +9,59 @@ import com.eulersbridge.iEngage.core.events.CreatedEvent;
 public class EventCreatedEvent extends CreatedEvent{
     private EventDetails eventDetails;
     private Long eventId;
+	private boolean institutionFound=true;
 
-    public EventCreatedEvent( Long eventId, EventDetails eventDetails) {
+    public EventCreatedEvent( Long eventId, EventDetails eventDetails)
+    {
         this.eventDetails = eventDetails;
         this.eventId = eventId;
     }
 
-    public EventCreatedEvent(Long eventId) {
+    public EventCreatedEvent(Long eventId)
+    {
         this.eventId = eventId;
     }
 
-    public EventDetails getEventDetails() {
+    public EventDetails getEventDetails()
+    {
         return eventDetails;
     }
 
-    public void setEventDetails(EventDetails eventDetails) {
+    public void setEventDetails(EventDetails eventDetails)
+    {
         this.eventDetails = eventDetails;
     }
 
-    public Long getEventId() {
+    public Long getEventId()
+    {
         return eventId;
     }
 
-    public void setEventId(Long eventId) {
+    public void setEventId(Long eventId)
+    {
         this.eventId = eventId;
     }
+
+	/**
+	 * @return the institutionFound
+	 */
+	public boolean isInstitutionFound()
+	{
+		return institutionFound;
+	}
+
+	/**
+	 * @param institutionFound the institutionFound to set
+	 */
+	public void setInstitutionFound(boolean institutionFound)
+	{
+		this.institutionFound = institutionFound;
+	}
+
+	public static EventCreatedEvent institutionNotFound(Long institutionId)
+	{
+		EventCreatedEvent evt=new EventCreatedEvent(institutionId);
+		evt.setInstitutionFound(false);
+		return evt;
+	}
 }
