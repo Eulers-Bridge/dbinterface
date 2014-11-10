1d13c96cd0cbdfa549fa607227ab0428e87813d6
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-11 10:43:31 +1100

    Reformatted.

--- src/test/java/com/eulersbridge/iEngage/core/events/elections/ElectionCreatedEventTest.java
+++ src/test/java/com/eulersbridge/iEngage/core/events/elections/ElectionCreatedEventTest.java
@@ -10,77 +10,91 @@ import static org.junit.Assert.*;
  * @author Yikai Gong
  */
 
-public class ElectionCreatedEventTest {
-    ElectionDetails electionDetails;
-    final Long electionId = new Long(0);
-    final String title = "election title";
-    final Long start = new Long(1);
-    final Long end = new Long(4);
-    final Long startVoting = new Long(2);
-    final Long endVoting = new Long(3);
-    ElectionCreatedEvent electionCreatedEvent;
+public class ElectionCreatedEventTest
+{
+	ElectionDetails electionDetails;
+	final Long electionId = new Long(0);
+	final String title = "election title";
+	final Long start = new Long(1);
+	final Long end = new Long(4);
+	final Long startVoting = new Long(2);
+	final Long endVoting = new Long(3);
+	ElectionCreatedEvent electionCreatedEvent;
 
-    @Before
-    public void setUp() throws Exception {
-        electionDetails = new ElectionDetails();
-        electionDetails.setElectionId(electionId);
-        electionDetails.setTitle(title);
-        electionDetails.setStart(start);
-        electionDetails.setEnd(end);
-        electionDetails.setStartVoting(startVoting);
-        electionDetails.setEndVoting(endVoting);
-        electionCreatedEvent = new ElectionCreatedEvent(electionId, electionDetails);
-    }
+	@Before
+	public void setUp() throws Exception
+	{
+		electionDetails = new ElectionDetails();
+		electionDetails.setElectionId(electionId);
+		electionDetails.setTitle(title);
+		electionDetails.setStart(start);
+		electionDetails.setEnd(end);
+		electionDetails.setStartVoting(startVoting);
+		electionDetails.setEndVoting(endVoting);
+		electionCreatedEvent = new ElectionCreatedEvent(electionId,
+				electionDetails);
+	}
 
-    @After
-    public void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception
+	{
 
-    }
+	}
 
-    @Test
-    public void testElectionCreatedEvent() throws Exception {
-        assertNotNull("electionCreatedEvent is null", electionCreatedEvent);
-        ElectionCreatedEvent electionCreatedEvent1 = new ElectionCreatedEvent(electionId);
-        assertNotNull("electionCreatedEvent is null", electionCreatedEvent1);
-    }
+	@Test
+	public void testElectionCreatedEvent() throws Exception
+	{
+		assertNotNull("electionCreatedEvent is null", electionCreatedEvent);
+		ElectionCreatedEvent electionCreatedEvent1 = new ElectionCreatedEvent(
+				electionId);
+		assertNotNull("electionCreatedEvent is null", electionCreatedEvent1);
+	}
 
-    @Test
-    public void testGetElectionId() throws Exception {
-        assertEquals("electionId does not match", electionId, electionCreatedEvent.getElectionId());
-    }
+	@Test
+	public void testGetElectionId() throws Exception
+	{
+		assertEquals("electionId does not match", electionId,
+				electionCreatedEvent.getElectionId());
+	}
 
-    @Test
-    public void testSetKey() throws Exception {
-        electionCreatedEvent.setKey(new Long(10));
-        assertEquals("id does not match", new Long(10), electionCreatedEvent.getElectionId());
-    }
+	@Test
+	public void testSetKey() throws Exception
+	{
+		electionCreatedEvent.setKey(new Long(10));
+		assertEquals("id does not match", new Long(10),
+				electionCreatedEvent.getElectionId());
+	}
 
-    @Test
-    public void testSetElectionDetails() throws Exception {
-        ElectionDetails electionDetails1 = new ElectionDetails();
-        electionDetails1.setElectionId(new Long(10));
-        electionDetails1.setTitle("new Title");
-        electionDetails1.setStart(start);
-        electionDetails1.setEnd(end);
-        electionDetails1.setStartVoting(startVoting);
-        electionDetails1.setEndVoting(endVoting);
+	@Test
+	public void testSetElectionDetails() throws Exception
+	{
+		ElectionDetails electionDetails1 = new ElectionDetails();
+		electionDetails1.setElectionId(new Long(10));
+		electionDetails1.setTitle("new Title");
+		electionDetails1.setStart(start);
+		electionDetails1.setEnd(end);
+		electionDetails1.setStartVoting(startVoting);
+		electionDetails1.setEndVoting(endVoting);
 
-        electionCreatedEvent.setElectionDetails(electionDetails1);
-        assertEquals("electionDetail does not match", electionDetails1, electionCreatedEvent.getElectionDetails());
-    }
+		electionCreatedEvent.setElectionDetails(electionDetails1);
+		assertEquals("electionDetail does not match", electionDetails1,
+				electionCreatedEvent.getElectionDetails());
+	}
 
-    @Test
-    public void testGetElectionDetails() throws Exception {
-        assertEquals("electionDetail does not match", electionDetails, electionCreatedEvent.getElectionDetails());
-    }
-    
-    @Test
-    public void testInsitutionNotFound() throws Exception
-    {
-    	Long id=5l;
-		ElectionCreatedEvent evt=ElectionCreatedEvent.institutionNotFound(id);
-    	assertEquals(id, evt.getElectionId());
-    	assertNull(evt.getElectionDetails());
-    	assertFalse(evt.isInstitutionFound());
-    }
+	@Test
+	public void testGetElectionDetails() throws Exception
+	{
+		assertEquals("electionDetail does not match", electionDetails,
+				electionCreatedEvent.getElectionDetails());
+	}
+
+	@Test
+	public void testInsitutionNotFound() throws Exception
+	{
+		Long id = 5l;
+		ElectionCreatedEvent evt = ElectionCreatedEvent.institutionNotFound(id);
+		assertEquals(id, evt.getElectionId());
+		assertNull(evt.getElectionDetails());
+		assertFalse(evt.isInstitutionFound());
+	}
 }
