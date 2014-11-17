1c6ed3ce84b61b3ba9f80b1f3d7e09fa13765dbc
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-17 11:31:50 +1100

    Fixed this a little so that it uses Mock. Need to revisit soon.

--- src/test/java/com/eulersbridge/iEngage/core/services/LikesEventHandlerTest.java
+++ src/test/java/com/eulersbridge/iEngage/core/services/LikesEventHandlerTest.java
@@ -2,49 +2,72 @@ package com.eulersbridge.iEngage.core.services;
 
 import com.eulersbridge.iEngage.core.events.likes.LikeableObjectLikesEvent;
 import com.eulersbridge.iEngage.core.events.likes.LikesLikeableObjectEvent;
-import com.eulersbridge.iEngage.database.domain.Fixture.DatabaseDataFixture;
+import com.eulersbridge.iEngage.core.events.photo.PhotoDetails;
+import com.eulersbridge.iEngage.core.events.photo.PhotoReadEvent;
+import com.eulersbridge.iEngage.core.events.photo.ReadPhotoEvent;
+import com.eulersbridge.iEngage.database.domain.Photo;
 import com.eulersbridge.iEngage.database.domain.User;
-import com.eulersbridge.iEngage.database.repository.UserMemoryRepository;
+import com.eulersbridge.iEngage.database.domain.Fixture.DatabaseDataFixture;
 import com.eulersbridge.iEngage.database.repository.UserRepository;
-import org.junit.After;
+
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-import java.util.Map;
-
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Yikai Gong
  */
 
-public class LikesEventHandlerTest {
-    LikesService likesService;
-    UserMemoryRepository userMemoryRepository;
+public class LikesEventHandlerTest
+{
+    private static Logger LOG = LoggerFactory.getLogger(LikesEventHandlerTest.class);
+
+    LikesService service;
 
     @Mock
     UserRepository userRepository;
 
+	/**
+	 * @throws java.lang.Exception
+	 */
     @Before
-    public void setUp() throws Exception {
-        Map<Long, User> users= DatabaseDataFixture.populateUsers();
-        userMemoryRepository = new UserMemoryRepository(users);
+    public void setUp() throws Exception
+    {
+		MockitoAnnotations.initMocks(this);
 
-        likesService = new LikesEventHandler(userMemoryRepository);
-        
-        assertNotNull("likesService is null", likesService);
+        service = new LikesEventHandler(userRepository);
     }
 
-    @After
-    public void tearDown() throws Exception {
+	/**
+	 * Test method for {@link com.eulersbridge.iEngage.core.services.LikesEventHandler#LikesEventHandler(com.eulersbridge.iEngage.database.repository.UserRepository)}.
+	 */
+	@Test
+	public final void testLikesEventHandler()
+	{
+		assertNotNull("Not yet implemented",service);
+	}
 
-    }
-
-    @Test
-    public void testLikes() throws Exception {
-        LikeableObjectLikesEvent likeableObjectLikesEvent = likesService.likes(new LikesLikeableObjectEvent(new Long(0)), Sort.Direction.DESC, 0, 10);
+//TODO Fix this test to be useful.
+	@Test
+    public void testLikesNullDataReturned() throws Exception
+    {
+		if (LOG.isDebugEnabled()) LOG.debug("FindingLikes()");
+		Page<User> testData=null;
+//		testData=DatabaseDataFixture.populatePhoto1();  Need to create testData returned.  There is an example of this in code.
+		when(userRepository.findByLikeableObjId(any(Long.class),any(Pageable.class))).thenReturn(testData);
+   	
+        LikeableObjectLikesEvent likeableObjectLikesEvent = service.likes(new LikesLikeableObjectEvent(new Long(0)), Sort.Direction.DESC, 0, 10);
         assertNotNull("likeableObjectLikesEvent is null", likeableObjectLikesEvent);
     }
 }
