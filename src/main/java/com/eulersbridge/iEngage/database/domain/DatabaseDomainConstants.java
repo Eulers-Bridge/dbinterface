ad0aab1140dfb164aff66485a6507d8925ddd27c
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-18 23:08:44 +1100

    Added photo album label.

--- src/main/java/com/eulersbridge/iEngage/database/domain/DatabaseDomainConstants.java
+++ src/main/java/com/eulersbridge/iEngage/database/domain/DatabaseDomainConstants.java
@@ -15,6 +15,7 @@ public class DatabaseDomainConstants
 	public static final String HAS_NEWS_FEED_LABEL = "HAS_NEWS_FEED";
 	public static final String HAS_ELECTION_LABEL = "HAS_ELECTION";
 	public static final String HAS_PHOTO_LABEL = "HAS_PHOTO";
+	public static final String HAS_PHOTO_ALBUM_LABEL = "HAS_PHOTO_ALBUM";
     public static final String HAS_POSITION_LABEL = "HAS_POSITION";
 	public static final String HAS_EVENT_LABEL = "HAS_EVENT";
 	
