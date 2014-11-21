50f4cafc677d4140b1944a8e0508d96dd6596507
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-21 22:08:49 +1100

    Implementing Owner

--- src/main/java/com/eulersbridge/iEngage/database/domain/Owner.java
+++ src/main/java/com/eulersbridge/iEngage/database/domain/Owner.java
@@ -3,12 +3,10 @@
  */
 package com.eulersbridge.iEngage.database.domain;
 
-import org.neo4j.graphdb.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.NodeEntity;
-import org.springframework.data.neo4j.annotation.RelatedTo;
 
 /**
  * @author Greg Newitt
@@ -46,6 +44,45 @@ public class Owner
 	public void setNodeId(Long nodeId)
 	{
 		this.nodeId = nodeId;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nodeId == null) ? 0 : nodeId.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Owner other = (Owner) obj;
+		if (nodeId == null)
+		{
+			if (other.nodeId != null) return false;
+		}
+		else if (!nodeId.equals(other.nodeId)) return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		return "Owner [nodeId=" + nodeId + "]";
 	}
 
 }
