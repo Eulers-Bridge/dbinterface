be66ead8a694070934ba5ea94f630661431afd34
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-21 12:08:23 +1100

    Extend coverage.

--- src/test/java/com/eulersbridge/iEngage/core/events/CreatedEventTest.java
+++ src/test/java/com/eulersbridge/iEngage/core/events/CreatedEventTest.java
@@ -1,6 +1,5 @@
 package com.eulersbridge.iEngage.core.events;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,21 +9,50 @@ import static org.junit.Assert.*;
  * @author Yikai Gong
  */
 
-public class CreatedEventTest {
-    CreatedEvent createdEvent = null;
+public class CreatedEventTest
+{
+    CreatedEvent createdEvent;
+    Details dets;
+	Long nodeId;
 
     @Before
-    public void setUp() throws Exception {
-        createdEvent = new CreatedEvent();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
+    public void setUp() throws Exception
+    {
+    	nodeId=23l;
+		dets=new Details(nodeId);
+        createdEvent = new CreatedEvent(dets);
     }
 
     @Test
-    public void testCreatedEvent() throws Exception {
+    public void testCreatedEvent() throws Exception
+    {
+    	createdEvent=new CreatedEvent();
         assertNotNull("createdEvent is null", createdEvent);
+    }
+    
+    @Test
+    public void testCreatedEventDetails() throws Exception
+    {
+        assertNotNull("createdEvent is null", createdEvent);
+        assertEquals(createdEvent.getDetails(),dets);
+    }
+    
+    @Test
+    public void testGetDetails() throws Exception
+    {
+        assertEquals(createdEvent.getDetails(),dets);
+    }
+    @Test
+    public void testSetDetails() throws Exception
+    {
+        assertEquals(createdEvent.getDetails(),dets);
+        Details dets2=new Details(nodeId);
+        createdEvent.setDetails(dets2);
+        assertEquals(createdEvent.getDetails(),dets);
+        assertEquals(createdEvent.getDetails(),dets2);
+        dets2=new Details(nodeId+1);
+        createdEvent.setDetails(dets2);
+        assertNotEquals(createdEvent.getDetails(),dets);
+        assertEquals(createdEvent.getDetails(),dets2);
     }
 }
