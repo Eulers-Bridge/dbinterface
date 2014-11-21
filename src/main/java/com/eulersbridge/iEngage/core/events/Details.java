fb71f4c002e644672573fe94db514e55a6dcc07d
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-21 12:08:07 +1100

    Add equals() and hashCode()

--- src/main/java/com/eulersbridge/iEngage/core/events/Details.java
+++ src/main/java/com/eulersbridge/iEngage/core/events/Details.java
@@ -38,6 +38,36 @@ public class Details
 	}
 
 	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nodeId == null) ? 0 : nodeId.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Details other = (Details) obj;
+		if (nodeId == null)
+		{
+			if (other.nodeId != null) return false;
+		}
+		else if (!nodeId.equals(other.nodeId)) return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
