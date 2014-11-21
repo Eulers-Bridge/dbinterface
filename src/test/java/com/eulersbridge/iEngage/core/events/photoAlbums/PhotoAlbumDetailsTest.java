aab5b60090bc2332ac91bc49dda5550d6e23dec3
Greg Newitt <greg.newitt@eulersbridge.com>
2014-11-21 12:08:48 +1100

    Implement unit test.

--- src/test/java/com/eulersbridge/iEngage/core/events/photoAlbums/PhotoAlbumDetailsTest.java
+++ src/test/java/com/eulersbridge/iEngage/core/events/photoAlbums/PhotoAlbumDetailsTest.java
@@ -5,120 +5,214 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.eulersbridge.iEngage.database.domain.PhotoAlbum;
+import com.eulersbridge.iEngage.database.domain.Fixture.DatabaseDataFixture;
+
 public class PhotoAlbumDetailsTest
 {
+	PhotoAlbumDetails dets;
+	PhotoAlbum photoAlbum;
 
 	@Before
 	public void setUp() throws Exception
 	{
-	}
-
-	@Test
-	public final void testHashCode()
-	{
-		fail("Not yet implemented"); // TODO
+		photoAlbum=DatabaseDataFixture.populatePhotoAlbum1();
+		dets=photoAlbum.toPhotoAlbumDetails();
 	}
 
 	@Test
 	public final void testPhotoAlbumDetails()
 	{
-		fail("Not yet implemented"); // TODO
+		assertNotNull("Not yet implemented",dets);
 	}
 
 	@Test
 	public final void testGetNodeId()
 	{
-		fail("Not yet implemented"); // TODO
+		assertEquals(photoAlbum.getNodeId(),dets.getNodeId());
 	}
 
 	@Test
 	public final void testSetNodeId()
 	{
-		fail("Not yet implemented"); // TODO
+		Long nodeId=232323l;
+		assertNotEquals(nodeId,dets.getNodeId());
+		dets.setNodeId(nodeId);
+		assertEquals(nodeId,dets.getNodeId());
 	}
 
 	@Test
 	public final void testGetName()
 	{
-		fail("Not yet implemented"); // TODO
+		assertEquals(photoAlbum.getName(),dets.getName());
 	}
 
 	@Test
 	public final void testSetName()
 	{
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testGetLocation()
-	{
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void testSetLocation()
-	{
-		fail("Not yet implemented"); // TODO
+		String name="Some name";
+		assertNotEquals(name,dets.getName());
+		dets.setName(name);
+		assertEquals(name,dets.getName());
 	}
 
 	@Test
 	public final void testGetDescription()
 	{
-		fail("Not yet implemented"); // TODO
+		assertEquals(photoAlbum.getDescription(),dets.getDescription());
 	}
 
 	@Test
 	public final void testSetDescription()
 	{
-		fail("Not yet implemented"); // TODO
+		String description="Some description";
+		assertNotEquals(description,dets.getDescription());
+		dets.setDescription(description);
+		assertEquals(description,dets.getDescription());
 	}
 
 	@Test
 	public final void testGetCreated()
 	{
-		fail("Not yet implemented"); // TODO
+		assertEquals(photoAlbum.getCreated(),dets.getCreated());
 	}
 
 	@Test
 	public final void testSetCreated()
 	{
-		fail("Not yet implemented"); // TODO
+		Long created=232323l;
+		assertNotEquals(created,dets.getCreated());
+		dets.setCreated(created);
+		assertEquals(created,dets.getCreated());
 	}
 
 	@Test
 	public final void testGetOwnerId()
 	{
-		fail("Not yet implemented"); // TODO
+		assertEquals(photoAlbum.getOwner().getNodeId(),dets.getOwnerId());
 	}
 
 	@Test
 	public final void testSetOwnerId()
 	{
-		fail("Not yet implemented"); // TODO
+		Long ownerId=232323l;
+		assertNotEquals(ownerId,dets.getOwnerId());
+		dets.setOwnerId(ownerId);
+		assertEquals(ownerId,dets.getOwnerId());
 	}
 
 	@Test
 	public final void testGetModified()
 	{
-		fail("Not yet implemented"); // TODO
+		assertEquals(photoAlbum.getModified(),dets.getModified());
 	}
 
 	@Test
 	public final void testSetModified()
 	{
-		fail("Not yet implemented"); // TODO
+		Long modified=232323l;
+		assertNotEquals(modified,dets.getModified());
+		dets.setModified(modified);
+		assertEquals(modified,dets.getModified());
+	}
+
+	@Test
+	public final void testHashCode()
+	{
+		PhotoAlbumDetails photoAlbumTest=DatabaseDataFixture.populatePhotoAlbum1().toPhotoAlbumDetails();
+		PhotoAlbumDetails detsTest=dets;
+		assertEquals(photoAlbumTest.hashCode(),photoAlbumTest.hashCode());
+		assertEquals(photoAlbumTest.hashCode(),detsTest.hashCode());
+		photoAlbumTest.setNodeId(null);
+		assertNotEquals(detsTest.hashCode(), photoAlbumTest.hashCode());
+		assertNotEquals(photoAlbumTest.hashCode(), detsTest.hashCode());
+		photoAlbumTest.setNodeId(null);
+		photoAlbumTest.setCreated(null);
+		assertNotEquals(detsTest.hashCode(), photoAlbumTest.hashCode());
+		assertNotEquals(photoAlbumTest.hashCode(), detsTest.hashCode());
+		photoAlbumTest.setCreated(detsTest.getCreated());
+		photoAlbumTest.setModified(null);
+		assertNotEquals(detsTest.hashCode(), photoAlbumTest.hashCode());
+		assertNotEquals(photoAlbumTest.hashCode(), detsTest.hashCode());
+		photoAlbumTest.setModified(detsTest.getModified());
+		photoAlbumTest.setOwnerId(null);
+		assertNotEquals(detsTest.hashCode(), photoAlbumTest.hashCode());
+		assertNotEquals(photoAlbumTest.hashCode(), detsTest.hashCode());
+		photoAlbumTest.setOwnerId(detsTest.getOwnerId());
+		photoAlbumTest.setName(null);
+		assertNotEquals(detsTest.hashCode(), photoAlbumTest.hashCode());
+		assertNotEquals(photoAlbumTest.hashCode(), detsTest.hashCode());
+		photoAlbumTest.setName(detsTest.getName());
+		photoAlbumTest.setDescription(null);
+		assertNotEquals(detsTest.hashCode(), photoAlbumTest.hashCode());
+		assertNotEquals(photoAlbumTest.hashCode(), detsTest.hashCode());
+		photoAlbumTest.setDescription(detsTest.getDescription());
 	}
 
 	@Test
 	public final void testEqualsObject()
 	{
-		fail("Not yet implemented"); // TODO
+        PhotoAlbumDetails photoAlbumDetails = photoAlbum.toPhotoAlbumDetails();
+        assertEquals("photoAlbumDetail does not match", photoAlbumDetails, dets);
+        
+        
+		PhotoAlbumDetails photoAlbumTest=null;
+		assertNotEquals(photoAlbumTest,photoAlbumDetails);
+		assertNotEquals(photoAlbumDetails,photoAlbumTest);
+		String notElection="";
+		assertNotEquals(photoAlbumDetails,notElection);
+		photoAlbumTest=DatabaseDataFixture.populatePhotoAlbum1().toPhotoAlbumDetails();
+		assertEquals(photoAlbumTest,photoAlbumTest);
+		assertEquals(photoAlbumTest,photoAlbumDetails);
+		photoAlbumTest.setNodeId(54l);
+		assertNotEquals(photoAlbumDetails, photoAlbumTest);
+		assertNotEquals(photoAlbumTest, photoAlbumDetails);
+		photoAlbumDetails.setNodeId(null);
+		photoAlbumTest.setNodeId(null);
+		photoAlbumTest.setCreated(photoAlbumDetails.getCreated());
+		assertEquals(photoAlbumDetails, photoAlbumTest);
+		assertEquals(photoAlbumTest, photoAlbumDetails);
+		photoAlbumTest.setName("Something else");
+		assertNotEquals(photoAlbumDetails, photoAlbumTest);
+		photoAlbumTest.setName(null);
+		assertNotEquals(photoAlbumDetails, photoAlbumTest);
+		assertNotEquals(photoAlbumTest, photoAlbumDetails);
+		photoAlbumTest.setName(photoAlbumDetails.getName());
+
+		photoAlbumTest.setDescription("Something else");
+		assertNotEquals(photoAlbumDetails, photoAlbumTest);
+		photoAlbumTest.setDescription(null);
+		assertNotEquals(photoAlbumDetails, photoAlbumTest);
+		assertNotEquals(photoAlbumTest, photoAlbumDetails);
+		photoAlbumTest.setDescription(photoAlbumDetails.getDescription());
+
+		photoAlbumTest.setCreated(54l);
+		PhotoAlbumDetails dets2=photoAlbumDetails;
+		assertNotEquals(dets2, photoAlbumTest);
+		photoAlbumTest.setCreated(null);
+		assertNotEquals(dets2, photoAlbumTest);
+		assertNotEquals(photoAlbumTest, dets2);
+		photoAlbumTest.setCreated(dets2.getCreated());
+		
+		photoAlbumTest.setModified(54l);
+		assertNotEquals(dets2, photoAlbumTest);
+		assertNotEquals(photoAlbumTest, dets2);
+		photoAlbumTest.setModified(null);
+		assertEquals(dets2, photoAlbumTest);
+		photoAlbumTest.setModified(dets2.getModified());
+		
+		photoAlbumTest.setOwnerId(54l);
+		assertNotEquals(dets2, photoAlbumTest);
+		photoAlbumTest.setOwnerId(null);
+		assertNotEquals(dets2, photoAlbumTest);
+		assertNotEquals(photoAlbumTest, dets2);
+		photoAlbumTest.setOwnerId(dets2.getOwnerId());
 	}
 
 	@Test
 	public final void testToString()
 	{
-		fail("Not yet implemented"); // TODO
+		assertNotNull("Not yet implemented",dets.toString());
 	}
 
 }
