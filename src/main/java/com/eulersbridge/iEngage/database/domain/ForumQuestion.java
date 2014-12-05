506c07d12b3a6f2b376120dbd8b0394301309f11
Greg Newitt <greg.newitt@eulersbridge.com>
2014-12-05 13:20:33 +1100

    Formatting

--- src/main/java/com/eulersbridge/iEngage/database/domain/ForumQuestion.java
+++ src/main/java/com/eulersbridge/iEngage/database/domain/ForumQuestion.java
@@ -11,65 +11,78 @@ import org.springframework.data.neo4j.annotation.NodeEntity;
  */
 
 @NodeEntity
-public class ForumQuestion {
-    @GraphId private Long forumQuestionId;
-    private String question;
+public class ForumQuestion
+{
+	@GraphId
+	private Long forumQuestionId;
+	private String question;
 
-    private static Logger LOG = LoggerFactory.getLogger(ForumQuestion.class);
+	private static Logger LOG = LoggerFactory.getLogger(ForumQuestion.class);
 
-    public ForumQuestion(){
-        if (LOG.isTraceEnabled()) LOG.trace("Constructor");
-    }
+	public ForumQuestion()
+	{
+		if (LOG.isTraceEnabled()) LOG.trace("Constructor");
+	}
 
-    public static ForumQuestion fromForumQuestionDetails(ForumQuestionDetails forumQuestionDetails){
-        if (LOG.isTraceEnabled()) LOG.trace("fromForumQuestionDetails()");
-        ForumQuestion forumQuestion = new ForumQuestion();
-        if (LOG.isTraceEnabled()) LOG.trace("forumQuestionDetails "+forumQuestionDetails);
-        forumQuestion.setForumQuestionId(forumQuestionDetails.getForumQuestionId());
-        forumQuestion.setQuestion(forumQuestionDetails.getQuestion());
+	public static ForumQuestion fromForumQuestionDetails(
+			ForumQuestionDetails forumQuestionDetails)
+	{
+		if (LOG.isTraceEnabled()) LOG.trace("fromForumQuestionDetails()");
+		ForumQuestion forumQuestion = new ForumQuestion();
+		if (LOG.isTraceEnabled())
+			LOG.trace("forumQuestionDetails " + forumQuestionDetails);
+		forumQuestion.setForumQuestionId(forumQuestionDetails
+				.getForumQuestionId());
+		forumQuestion.setQuestion(forumQuestionDetails.getQuestion());
 
-        if (LOG.isTraceEnabled()) LOG.trace("forumQuestion "+forumQuestion);
-        return forumQuestion;
-    }
+		if (LOG.isTraceEnabled()) LOG.trace("forumQuestion " + forumQuestion);
+		return forumQuestion;
+	}
 
-    public ForumQuestionDetails toForumQuestionDetails(){
-        if (LOG.isTraceEnabled()) LOG.trace("toForumQuestionDetails()");
-        ForumQuestionDetails forumQuestionDetails = new ForumQuestionDetails();
-        if (LOG.isTraceEnabled()) LOG.trace("forumQuestion "+this);
-        forumQuestionDetails.setForumQuestionId(this.getForumQuestionId());
-        forumQuestionDetails.setQuestion(getQuestion());
+	public ForumQuestionDetails toForumQuestionDetails()
+	{
+		if (LOG.isTraceEnabled()) LOG.trace("toForumQuestionDetails()");
+		ForumQuestionDetails forumQuestionDetails = new ForumQuestionDetails();
+		if (LOG.isTraceEnabled()) LOG.trace("forumQuestion " + this);
+		forumQuestionDetails.setForumQuestionId(this.getForumQuestionId());
+		forumQuestionDetails.setQuestion(getQuestion());
 
-        if (LOG.isTraceEnabled()) LOG.trace("forumQuestionDetails; "+ forumQuestionDetails);
-        return forumQuestionDetails;
-    }
+		if (LOG.isTraceEnabled())
+			LOG.trace("forumQuestionDetails; " + forumQuestionDetails);
+		return forumQuestionDetails;
+	}
 
-    @Override
-    public String toString()
-    {
-        StringBuffer buff=new StringBuffer("[ nodeId = ");
-        String retValue;
-        buff.append(getForumQuestionId());
-        buff.append(", question = ");
-        buff.append(getQuestion());
-        buff.append(" ]");
-        retValue=buff.toString();
-        if (LOG.isDebugEnabled()) LOG.debug("toString() = "+retValue);
-        return retValue;
-    }
+	@Override
+	public String toString()
+	{
+		StringBuffer buff = new StringBuffer("[ nodeId = ");
+		String retValue;
+		buff.append(getForumQuestionId());
+		buff.append(", question = ");
+		buff.append(getQuestion());
+		buff.append(" ]");
+		retValue = buff.toString();
+		if (LOG.isDebugEnabled()) LOG.debug("toString() = " + retValue);
+		return retValue;
+	}
 
-    public Long getForumQuestionId() {
-        return forumQuestionId;
-    }
+	public Long getForumQuestionId()
+	{
+		return forumQuestionId;
+	}
 
-    public void setForumQuestionId(Long forumQuestionId) {
-        this.forumQuestionId = forumQuestionId;
-    }
+	public void setForumQuestionId(Long forumQuestionId)
+	{
+		this.forumQuestionId = forumQuestionId;
+	}
 
-    public String getQuestion() {
-        return question;
-    }
+	public String getQuestion()
+	{
+		return question;
+	}
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
+	public void setQuestion(String question)
+	{
+		this.question = question;
+	}
 }
