b59c425b5c33ddeb50f3bd69c3aea90886d58928
darcular <yikaig@student.unimelb.edu.au>
2014-12-14 12:25:28 +1100

    Add TaskRepository

--- src/main/java/com/eulersbridge/iEngage/database/repository/TaskRepository.java
+++ src/main/java/com/eulersbridge/iEngage/database/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.eulersbridge.iEngage.database.repository;
 
+import com.eulersbridge.iEngage.database.domain.Task;
+import org.springframework.data.neo4j.repository.GraphRepository;
+
 /**
  * @author Yikai Gong
  */
 
-public interface TaskRepository {
+public interface TaskRepository extends GraphRepository<Task> {
 }
