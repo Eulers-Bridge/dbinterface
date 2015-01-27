bcd661a952626bcc2665406bffb1fe5ef9cb844c
Greg Newitt <greg.newitt@eulersbridge.com>
2015-01-27 22:29:12 +1100

    Getting equals() and hashcode() implemented.

--- src/main/java/com/eulersbridge/iEngage/database/domain/Position.java
+++ src/main/java/com/eulersbridge/iEngage/database/domain/Position.java
@@ -1,6 +1,7 @@
 package com.eulersbridge.iEngage.database.domain;
 
 import com.eulersbridge.iEngage.core.events.positions.PositionDetails;
+
 import org.neo4j.graphdb.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -102,4 +103,73 @@ public class Position {
     public void setElection(Election election) {
         this.election = election;
     }
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		if (this.nodeId!=null)
+		{
+			result = prime * result + nodeId.hashCode();
+		}
+		else
+		{
+			result = prime * result
+					+ ((description == null) ? 0 : description.hashCode());
+			result = prime * result
+					+ ((election == null) ? 0 : election.hashCode());
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			return result;
+		}
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Position other = (Position) obj;
+		if (nodeId != null)
+		{
+			if (nodeId.equals(other.nodeId))
+				return true;
+			else return false;
+		}
+		else
+		{
+			if (other.nodeId != null)
+				return false;
+
+			if (description == null)
+			{
+				if (other.description != null) return false;
+			}
+			else if (!description.equals(other.description)) return false;
+			if (election == null)
+			{
+				if (other.election != null) return false;
+			}
+			else if (!election.equals(other.election)) return false;
+			if (name == null)
+			{
+				if (other.name != null) return false;
+			}
+			else if (!name.equals(other.name)) return false;
+			if (nodeId == null)
+			{
+				if (other.nodeId != null) return false;
+			}
+			else if (!nodeId.equals(other.nodeId)) return false;
+		}
+		return true;
+	}
 }
