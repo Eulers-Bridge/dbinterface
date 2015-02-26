271a713265864be4d6408843730cd681a4be69ab
Greg Newitt <greg.newitt@eulersbridge.com>
2015-02-26 15:09:41 +1100

    Added hashcode() and equals()

--- src/main/java/com/eulersbridge/iEngage/database/domain/Badge.java
+++ src/main/java/com/eulersbridge/iEngage/database/domain/Badge.java
@@ -1,6 +1,7 @@
 package com.eulersbridge.iEngage.database.domain;
 
 import com.eulersbridge.iEngage.core.events.badge.BadgeDetails;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.neo4j.annotation.GraphId;
@@ -111,4 +112,65 @@ public class Badge {
     public void setXpValue(Long xpValue) {
         this.xpValue = xpValue;
     }
+    
+    /*
+     * (non-Javadoc)
+     *
+     * @see java.lang.Object#hashCode()
+     */
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		if (this.nodeId != null)
+		{
+			result = prime * result + nodeId.hashCode();
+		}
+		else
+		{
+			result = prime * result + ((name == null) ? 0 : name.hashCode());
+			result = prime * result
+					+ ((xpValue == null) ? 0 : xpValue.hashCode());
+		}
+		return result;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		Badge other = (Badge) obj;
+		if (nodeId != null)
+		{
+			if (nodeId.equals(other.nodeId))
+				return true;
+			else return false;
+		}
+		else
+		{
+			if (other.nodeId != null) return false;
+			
+			if (name == null)
+			{
+				if (other.name != null) return false;
+			}
+			else if (!name.equals(other.name)) return false;
+			
+			if (xpValue == null)
+			{
+				if (other.xpValue != null) return false;
+			}
+			else if (!xpValue.equals(other.xpValue)) return false;
+		}
+		return true;
+	}
+
 }
