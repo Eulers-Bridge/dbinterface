7703d96b96d041f7a9223be3c032c95096fa8eb8
Greg Newitt <greg.newitt@eulersbridge.com>
2015-04-09 11:20:42 +1000

    Add the findRequests queries.

--- src/main/java/com/eulersbridge/iEngage/database/repository/ContactRequestRepository.java
+++ src/main/java/com/eulersbridge/iEngage/database/repository/ContactRequestRepository.java
@@ -3,6 +3,8 @@
  */
 package com.eulersbridge.iEngage.database.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.data.repository.query.Param;
@@ -19,5 +21,11 @@ public interface ContactRequestRepository extends
 {
 	@Query ("MATCH (u:`"+DatabaseDomainConstants.USER+"`)-[r:`"+DatabaseDomainConstants.CONTACT_REQUEST_LABEL+"`]-(c:`"+DatabaseDomainConstants.CONTACT_REQUEST+"`) where id(u)={userId} and c.contactDetails={contactInfo} RETURN c")
 	ContactRequest findContactRequestByUserIdContactInfo(@Param("userId")Long userId, @Param("contactInfo")String contactInfo);
+
+	@Query ("MATCH (u:`"+DatabaseDomainConstants.USER+"`), (c:`"+DatabaseDomainConstants.CONTACT_REQUEST+"`) where id(u)={userId} and ((c.contactDetails=u.contactNumber)or(c.contactDetails=u.email)) RETURN c")
+	Page<ContactRequest> findReceivedRequestsByUserId(@Param("userId")Long userId, Pageable pageable);
+
+	@Query ("MATCH (u:`"+DatabaseDomainConstants.USER+"`)-[r:`"+DatabaseDomainConstants.CONTACT_REQUEST_LABEL+"`]-(c:`"+DatabaseDomainConstants.CONTACT_REQUEST+"`) where id(u)={userId} RETURN c")
+	Page<ContactRequest> findSentRequests(@Param("userId")Long userId, Pageable pageable);
 
 }
