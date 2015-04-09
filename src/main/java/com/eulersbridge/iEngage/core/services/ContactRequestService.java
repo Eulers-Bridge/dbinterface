b3a728f3584f7aa997bf0b3b05a873937fe6debc
Greg Newitt <greg.newitt@eulersbridge.com>
2015-04-09 11:19:37 +1000

    Creating ContactRequestService.

--- src/main/java/com/eulersbridge/iEngage/core/services/ContactRequestService.java
+++ src/main/java/com/eulersbridge/iEngage/core/services/ContactRequestService.java
@@ -3,10 +3,14 @@
  */
 package com.eulersbridge.iEngage.core.services;
 
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.access.prepost.PreAuthorize;
 
+import com.eulersbridge.iEngage.core.events.AllReadEvent;
 import com.eulersbridge.iEngage.core.events.CreatedEvent;
+import com.eulersbridge.iEngage.core.events.ReadAllEvent;
 import com.eulersbridge.iEngage.core.events.ReadEvent;
+import com.eulersbridge.iEngage.core.events.UpdateEvent;
 import com.eulersbridge.iEngage.core.events.UpdatedEvent;
 import com.eulersbridge.iEngage.core.events.contactRequest.AcceptContactRequestEvent;
 import com.eulersbridge.iEngage.core.events.contactRequest.CreateContactRequestEvent;
@@ -29,5 +33,11 @@ public interface ContactRequestService
 	
 	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
 	public UpdatedEvent acceptContactRequest(AcceptContactRequestEvent acceptContactRequestEvent);
+
+	UpdatedEvent rejectContactRequest(UpdateEvent acceptContactRequestEvent);
+
+	public AllReadEvent readContactRequestsReceived(ReadAllEvent readAllEvent, Direction sortDirection, int pageNumber, int pageLength);
+	
+	public AllReadEvent readContactRequestsMade(ReadAllEvent readAllEvent, Direction sortDirection, int pageNumber, int pageLength);
 
 }
