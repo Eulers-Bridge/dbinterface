f0e2d497607f5288a2133c2121fa2b80a9d54b38
Greg Newitt <greg.newitt@eulersbridge.com>
2015-04-16 19:57:16 +1000

    Added a failed field to UpdatedEvent

--- src/main/java/com/eulersbridge/iEngage/core/events/UpdatedEvent.java
+++ src/main/java/com/eulersbridge/iEngage/core/events/UpdatedEvent.java
@@ -5,6 +5,7 @@ public class UpdatedEvent
 	private Long nodeId;
 	private Details details;
 	protected boolean entityFound = true;
+	protected boolean failed = false;
 
 	public UpdatedEvent(Long nodeId, Details details)
 	{
@@ -32,10 +33,25 @@ public class UpdatedEvent
 		return entityFound;
 	}
 
+	/**
+	 * @return the failed
+	 */
+	public boolean isFailed()
+	{
+		return failed;
+	}
+
 	public static UpdatedEvent notFound(Long id)
 	{
 		UpdatedEvent updatedEvent = new UpdatedEvent(id);
 		updatedEvent.entityFound = false;
+		return updatedEvent;
+	}
+	
+	public static UpdatedEvent failed(Long id)
+	{
+		UpdatedEvent updatedEvent = new UpdatedEvent(id);
+		updatedEvent.failed = true;
 		return updatedEvent;
 	}
 	
@@ -46,6 +62,6 @@ public class UpdatedEvent
 	public String toString()
 	{
 		return "UpdatedEvent [nodeId=" + nodeId + ", details=" + details
-				+ ", entityFound=" + entityFound + "]";
+				+ ", entityFound=" + entityFound + ", failed=" + failed + "]";
 	}
 }
