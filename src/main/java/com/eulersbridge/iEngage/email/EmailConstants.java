8624876e1c2f0411eca87f1ad8562488b5020c5f
Greg Newitt <greg.newitt@eulersbridge.com>
2015-08-10 16:39:35 +1000

    Use new email template.

--- src/main/java/com/eulersbridge/iEngage/email/EmailConstants.java
+++ src/main/java/com/eulersbridge/iEngage/email/EmailConstants.java
@@ -3,5 +3,5 @@ package com.eulersbridge.iEngage.email;
 public class EmailConstants 
 {
 	public static final int DEFAULT_EXPIRY_TIME_IN_MINS = 60 * 24 * 7; //seven days
-	public static final String EmailVerificationTemplate = "/validateEmailBody.vm";
+	public static final String EmailVerificationTemplate = "/sign_up_in_progress.vm";
 }
