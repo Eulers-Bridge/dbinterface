98d8d2416ff88583d3e2934389a843924d4e5133
Yikai Gong <yikaig@student.unimelb.edu.au>
2017-05-13 17:20:02 +1000

    Updated CandidateRepository for extending load-depth to 1

--- src/main/java/com/eulersbridge/iEngage/database/repository/CandidateRepository.java
+++ src/main/java/com/eulersbridge/iEngage/database/repository/CandidateRepository.java
@@ -17,14 +17,14 @@ public interface CandidateRepository extends GraphRepository<Candidate>
 
 	@Query("Match (n:`"+ DataConstants.ELECTION+"`)-[r:"+ DataConstants.HAS_POSITION_LABEL+
 			"]-(p:`"+ DataConstants.POSITION+"`)-[q:"+ DataConstants.HAS_CANDIDATE_LABEL+
-				"]-(e:`"+ DataConstants.CANDIDATE+"`) where id(n)={electionId} return e")
+				"]-(e:`"+ DataConstants.CANDIDATE+"`) where id(n)={electionId} return distinct (e)-[*0..1]-(), e")
 	Page<Candidate> findByElectionId(@Param("electionId")Long electionId, Pageable pageable);
 
 	@Query("Match (n:`"+ DataConstants.POSITION+"`)-[r:"+ DataConstants.HAS_CANDIDATE_LABEL+
-			"]-(e:`"+ DataConstants.CANDIDATE+"`) where id(n)={positionId} return e")
+			"]-(e:`"+ DataConstants.CANDIDATE+"`) where id(n)={positionId} return distinct (e)-[*0..1]-(), e")
 	Page<Candidate> findByPositionId(@Param("positionId")Long positionId, Pageable pageable);
 
-	@Query("Match (n:`"+ DataConstants.CANDIDATE+"`)-[r:"+ DataConstants.IS_ON_TICKET_LABEL+
-			"]-(e:`"+ DataConstants.TICKET+"`) where id(e)={ticketId} return n")
+	@Query("Match (e:`"+ DataConstants.CANDIDATE+"`)-[r:"+ DataConstants.IS_ON_TICKET_LABEL+
+			"]-(t:`"+ DataConstants.TICKET+"`) where id(t)={ticketId} return distinct (e)-[*0..1]-(), e")
 	Page<Candidate> findByTicketId(@Param("ticketId")Long ticketId, Pageable pageable);
 }
